e6f792aa7b2911d65a1920cc23da9d2b1ea3632a
prgrmz07 <prgrmz07@163.com>
2023-07-18 21:38:43 +0800

    CodeStyleChecker  : 遍历语句 ，在语句周围插入/**/  ： 开发中，输出各种语句类型

--- lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
@@ -13,6 +13,20 @@ using namespace clang;
 // CodeStyleCheckerVisitor implementation
 //-----------------------------------------------------------------------------
 
+
+bool shouldInsert(clang::Stmt::StmtClass & stmtClass){
+  switch (stmtClass) {
+    case clang::Stmt::CompoundStmtClass:
+    case clang::Stmt::ImplicitCastExprClass: //隐式类型转换
+      return false;
+
+    case clang::Stmt::DeclStmtClass:
+    case clang::Stmt::CallExprClass:
+      return true;
+  }
+  return true;
+}
+
 /**遍历语句
  *
  * @param S
@@ -22,13 +36,123 @@ bool CodeStyleCheckerVisitor::VisitStmt(clang::Stmt *S){
   //ref:  https://stackoverflow.com/questions/40596195/pretty-print-statement-to-string-in-clang/40599057#40599057
   auto range=S->getSourceRange();
   auto cRange=CharSourceRange::getCharRange(range);
-  llvm::StringRef strRef=Lexer::getSourceText(cRange,mRewriter.getSourceMgr(),mRewriter.getLangOpts());
+  llvm::StringRef strRefStmt=Lexer::getSourceText(cRange, mRewriter.getSourceMgr(), mRewriter.getLangOpts());
 
-  std::cout << strRef.str() << std::endl;
+  auto strStmt=strRefStmt.str();
 
-  mRewriter.InsertTextAfter(S->getEndLoc(),"/**/");
+  auto stmtClass = S->getStmtClass();
+  auto stmtClassName = S->getStmtClassName();
+
+
+  std::cout << "[#" << strStmt << "#]:{#" << stmtClassName << "#}" << std::endl;
+
+  bool isCompoundStmtClass= ( stmtClass==clang::Stmt::CompoundStmtClass);
+  if(shouldInsert(stmtClass)){
+//  mRewriter.InsertTextAfter(S->getEndLoc(),"/**/");
+    mRewriter.InsertTextBefore(S->getBeginLoc(),"/**/");
+  }
   return true;
 }
+
+/*
+[#{
+  int age;
+  printf("input age:");
+  scanf("%d",&age);
+  printf("your age:%d\n",age);
+
+  int alive=false;
+  int secret[100];
+  for(int k =0; k <100; k++){
+    if (secret[k] < age){
+      alive=true;
+      break;
+    }
+  }
+
+  printf("are you still be alive?%d\n",alive);
+
+  return 0;
+
+#]:{#CompoundStmt#}
+[#int age#]:{#DeclStmt#}
+[#printf("input age:"#]:{#CallExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#StringLiteral#}
+[#scanf("%d",&age#]:{#CallExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#StringLiteral#}
+[#&#]:{#UnaryOperator#}
+[##]:{#DeclRefExpr#}
+[#printf("your age:%d\n",age#]:{#CallExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#StringLiteral#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[#int alive=false#]:{#DeclStmt#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#CXXBoolLiteralExpr#}
+[#int secret[100]#]:{#DeclStmt#}
+[##]:{#IntegerLiteral#}
+[#for(int k =0; k <100; k++){
+    if (secret[k] < age){
+      alive=true;
+      break;
+    }
+  #]:{#ForStmt#}
+[#int k =0#]:{#DeclStmt#}
+[##]:{#IntegerLiteral#}
+[#k <#]:{#BinaryOperator#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#IntegerLiteral#}
+[#k#]:{#UnaryOperator#}
+[##]:{#DeclRefExpr#}
+[#{
+    if (secret[k] < age){
+      alive=true;
+      break;
+    }
+  #]:{#CompoundStmt#}
+[#if (secret[k] < age){
+      alive=true;
+      break;
+    #]:{#IfStmt#}
+[#secret[k] < #]:{#BinaryOperator#}
+[#secret[k#]:{#ImplicitCastExpr#}
+[#secret[k#]:{#ArraySubscriptExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[#{
+      alive=true;
+      break;
+    #]:{#CompoundStmt#}
+[#alive=#]:{#BinaryOperator#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#CXXBoolLiteralExpr#}
+[##]:{#BreakStmt#}
+[#printf("are you still be alive?%d\n",alive#]:{#CallExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#StringLiteral#}
+[##]:{#ImplicitCastExpr#}
+[##]:{#DeclRefExpr#}
+[#return #]:{#ReturnStmt#}
+[##]:{#IntegerLiteral#}
+ */
+
 
 bool CodeStyleCheckerVisitor::VisitCXXRecordDecl(CXXRecordDecl *Decl) {
   // Skip anonymous records, e.g. unions:
