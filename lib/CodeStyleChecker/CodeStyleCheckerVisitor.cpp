c492ac9263d4eb06f879f141af470fcf4d627223
prgrmz07 <prgrmz07@163.com>
2023-07-19 11:56:35 +0800

    case独立开；调用语句CallExpr 的父节点类型判断

--- lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
@@ -18,29 +18,87 @@ using namespace clang;
 bool shouldInsert(clang::Stmt *S,ASTNodeKind& parent0NodeKind){
 //clang::Stmt::StmtClass & stmtClass
   clang::Stmt::StmtClass stmtClass = S->getStmtClass();
-  switch (stmtClass) {
-    case clang::Stmt::CompoundStmtClass:
-    case clang::Stmt::ImplicitCastExprClass: //隐式类型转换
-    case clang::Stmt::StringLiteralClass:
-    case clang::Stmt::IntegerLiteralClass:
-    case clang::Stmt::DeclRefExprClass://存疑,待找到实例以验证
+  switch (stmtClass) {//switch开始
+    //{不插入时钟语句概率大的情况
+    case clang::Stmt::CompoundStmtClass:{
       return false;
+    }
+    case clang::Stmt::ImplicitCastExprClass:{ //隐式类型转换
+      return false;
+    }
+    case clang::Stmt::StringLiteralClass:{
+      return false;
+    }
+    case clang::Stmt::IntegerLiteralClass:{
+      return false;
+    }
+    case clang::Stmt::DeclRefExprClass:{//存疑,待找到实例以验证
+      return false;
+    }
+    //}
 
+    //{插入时钟语句概率大的情况
     case clang::Stmt::DeclStmtClass:{
       auto forStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::ForStmt>();
       if(parent0NodeKind.isSame(forStmtAstNodeKind) ){
         //如果当前语句S的父亲节点是for语句头，则不插入时钟语句.
         return false;
       }
+      return true;
     }
-    case clang::Stmt::CallExprClass:
-    case clang::Stmt::ForStmtClass://for循环整体
+    case clang::Stmt::CallExprClass:{
+      {
+      ASTNodeKind compoundStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::CompoundStmt>();
+      if(parent0NodeKind.isSame(compoundStmtAstNodeKind)){
+        //如果调用语句CallExpr的父亲节点是组合语句CompoundStmt，则插入时钟语句. 举例如下:
+        // 组合语句CompoundStmt: "{ int age; func1();}" , 其中 "func1();" 是调用语句CallExpr.
+        return true;
+      }
+      }
+
+      {
+      ASTNodeKind varDeclAstNodeKind=ASTNodeKind::getFromNodeKind<clang::VarDecl>();
+      if(parent0NodeKind.isSame(varDeclAstNodeKind)){
+        //如果调用语句CallExpr的父亲节点是变量声明语句VarDecl，则不插入时钟语句. 举例如下:
+        // 变量声明语句VarDecl: "float cost=calcCost(3,false);" , 其中 "calcCost(3,false)" 是调用语句CallExpr.
+        return false;
+      }
+      }
+
+      {
+      ASTNodeKind binaryOperatorAstNodeKind=ASTNodeKind::getFromNodeKind<clang::BinaryOperator>();
+      if(parent0NodeKind.isSame(binaryOperatorAstNodeKind)){
+        //如果调用语句CallExpr的父亲节点是 二元操作符BinaryOperator，则不插入时钟语句. 举例如下:
+        // 二元操作符BinaryOperator 语句: "name=getName(true);" , 其中 "getName(true)" 是调用语句CallExpr, 二元操作符BinaryOperator 这里是指 赋值号"="
+        return false;
+      }
+      }
+
+      {
+      ASTNodeKind ifStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::IfStmt>();
+      if(parent0NodeKind.isSame(ifStmtAstNodeKind)){
+        //如果调用语句CallExpr的父亲节点是 判断语句IfStmt，则不插入时钟语句，否则破坏了原来意思. 举例如下:
+        // 判断语句IfStmt  : "if(done) do_job();" , 其中 "do_job()" 是调用语句CallExpr
+        return false;
+      }
+      }
+
+      return true;
+    }
+    case clang::Stmt::ForStmtClass: {//for循环整体
+      return true;
+    }
 //    case clang::Stmt::UnaryOperatorClass://一元操作符语句,  这里 暂时不插入. 因为需要知道当前是在for循环第3位置 , 还是单独一条语句. for循环第3位置前插入 分割符只能是逗号, 单独一条语句前插入 分割符只能是分号.
 //    case clang::Stmt::BinaryOperatorClass://二元操作符语句，暂时不插入。 需要看其内部组成和外部包裹，比如若是被if()包裹 肯定其前肯定不能插入，如果是单独一条语句 其前可以插入。
-    case clang::Stmt::IfStmtClass://if语句整体
-    case clang::Stmt::ReturnStmtClass:
+    case clang::Stmt::IfStmtClass: {//if语句整体
       return true;
-  }
+    }
+    case clang::Stmt::ReturnStmtClass:{
+      return true;
+    }
+    //}
+  }//switch结束
+
 
   //默认不插入
   return false;
