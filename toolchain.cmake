126ecf7f833ba029449c1619eece8be0a9a5ee2d
prgrmz07 <prgrmz07@163.com>
2023-07-19 22:33:13 +0800

    换llvm版本:  llvm  11.1.0  -->  llvm 8.0.1

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,6 +1,6 @@
 #ref: https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/CrossCompiling
-#cmake -DCT_Clang_INSTALL_DIR=/llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCURSES_LIBRARY=/lib64/libncurses.so.6 -DCURSES_INCLUDE_PATH=/usr/include/   -DCMAKE_EXPORT_COMPILE_COMMANDS=True   -DCMAKE_C_COMPILER=/llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/bin/clang -DCMAKE_CXX_COMPILER=/llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/bin/clang++    -DLLVM_DIR=/llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10 ..
+#cmake -DCT_Clang_INSTALL_DIR=/llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04/  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCURSES_LIBRARY=/lib64/libncurses.so.6 -DCURSES_INCLUDE_PATH=/usr/include/   -DCMAKE_EXPORT_COMPILE_COMMANDS=True   -DCMAKE_C_COMPILER=/llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04/bin/clang -DCMAKE_CXX_COMPILER=/llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04/bin/clang++    -DLLVM_DIR=/llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04 ..
 
 #cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake ..
-SET(CMAKE_C_COMPILER   /llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/bin/clang)
-SET(CMAKE_CXX_COMPILER /llvm_release_home/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/bin/clang++)
+SET(CMAKE_C_COMPILER   /llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04/bin/clang)
+SET(CMAKE_CXX_COMPILER /llvm_release_home/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04/bin/clang++)
