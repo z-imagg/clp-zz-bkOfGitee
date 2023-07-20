0e8a30b955938233fb77cb65d22ec26c303fbf91
prgrmz07 <prgrmz07@163.com>
2023-07-20 12:45:40 +0800

    删掉原始代码中 大量check：这些check会报错，导致不能继续

--- include/CodeStyleChecker/CodeStyleCheckerVisitor.h
+++ include/CodeStyleChecker/CodeStyleCheckerVisitor.h
@@ -349,13 +349,4 @@ public:
 private:
     clang::ASTContext *Ctx;
 
-    // Checks whether the name in Decl contains any `_`. Issues a warnning if it
-    // does.
-    void checkNoUnderscoreInName(clang::NamedDecl *Decl);
-    // Checks whether the name in Decl starts with a lower case letter. Issues a
-    // warning if not.
-    void checkNameStartsWithLowerCase(clang::NamedDecl *Decl);
-    // Checks whether the name in Decl starts with an upper case letter. Issues a
-    // warning if not.
-    void checkNameStartsWithUpperCase(clang::NamedDecl *Decl);
 };
--- lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
@@ -317,117 +317,18 @@ bool CodeStyleCheckerVisitor::VisitStmt(clang::Stmt *S){
 
 
 bool CodeStyleCheckerVisitor::VisitCXXRecordDecl(CXXRecordDecl *Decl) {
-  // Skip anonymous records, e.g. unions:
-  //    * https://en.cppreference.com/w/cpp/language/union
-  if (0 == Decl->getNameAsString().size())
-    return true;
-
-  checkNameStartsWithUpperCase(Decl);
-  checkNoUnderscoreInName(Decl);
   return true;
 }
 
 bool CodeStyleCheckerVisitor::VisitFunctionDecl(FunctionDecl *Decl) {
-  // Skip user-defined conversion operators/functions:
-  //    * https://en.cppreference.com/w/cpp/language/cast_operator
-  if (isa<CXXConversionDecl>(Decl))
-    return true;
-
-  checkNameStartsWithLowerCase(Decl);
-  checkNoUnderscoreInName(Decl);
   return true;
 }
 
 bool CodeStyleCheckerVisitor::VisitVarDecl(VarDecl *Decl) {
-  // Skip anonymous function parameter declarations
-  if (isa<ParmVarDecl>(Decl) && (0 == Decl->getNameAsString().size()))
-    return true;
-
-  checkNameStartsWithUpperCase(Decl);
-  checkNoUnderscoreInName(Decl);
   return true;
 }
 
 bool CodeStyleCheckerVisitor::VisitFieldDecl(FieldDecl *Decl) {
-  // Skip anonymous bit-fields:
-  //  * https://en.cppreference.com/w/c/language/bit_field
-  if (0 == Decl->getNameAsString().size())
-    return true;
-
-  checkNameStartsWithUpperCase(Decl);
-  checkNoUnderscoreInName(Decl);
 
   return true;
-}
-
-void CodeStyleCheckerVisitor::checkNoUnderscoreInName(NamedDecl *Decl) {
-  auto Name = Decl->getNameAsString();
-  size_t underscorePos = Name.find('_');
-
-  if (underscorePos == StringRef::npos)
-    return;
-
-  std::string Hint = Name;
-  auto end_pos = std::remove(Hint.begin(), Hint.end(), '_');
-  Hint.erase(end_pos, Hint.end());
-
-  FixItHint FixItHint = FixItHint::CreateReplacement(
-          SourceRange(Decl->getLocation(),
-                      Decl->getLocation().getLocWithOffset(Name.size())),
-          Hint);
-
-  DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
-  unsigned DiagID = DiagEngine.getCustomDiagID(DiagnosticsEngine::Warning,
-                                               "`_` in names is not allowed");
-  SourceLocation UnderscoreLoc =
-          Decl->getLocation().getLocWithOffset(underscorePos);
-  DiagEngine.Report(UnderscoreLoc, DiagID).AddFixItHint(FixItHint);
-}
-
-void CodeStyleCheckerVisitor::checkNameStartsWithLowerCase(NamedDecl *Decl) {
-  auto Name = Decl->getNameAsString();
-  char FirstChar = Name[0];
-
-  // The actual check
-  if (isLowercase(FirstChar))
-    return;
-
-  // Construct the hint
-  std::string Hint = Name;
-  Hint[0] = toLowercase(FirstChar);
-  FixItHint FixItHint = FixItHint::CreateReplacement(
-          SourceRange(Decl->getLocation(),
-                      Decl->getLocation().getLocWithOffset(Name.size())),
-          Hint);
-
-  // Construct the diagnostic
-  DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
-  unsigned DiagID = DiagEngine.getCustomDiagID(
-          DiagnosticsEngine::Warning,
-          "Function names should start with lower-case letter");
-  DiagEngine.Report(Decl->getLocation(), DiagID) << FixItHint;
-}
-
-void CodeStyleCheckerVisitor::checkNameStartsWithUpperCase(NamedDecl *Decl) {
-  auto Name = Decl->getNameAsString();
-  char FirstChar = Name[0];
-
-  // The actual check
-  if (isUppercase(FirstChar))
-    return;
-
-  // Construct the hint
-  std::string Hint = Name;
-  Hint[0] = toUppercase(FirstChar);
-  FixItHint FixItHint = FixItHint::CreateReplacement(
-          SourceRange(Decl->getLocation(),
-                      Decl->getLocation().getLocWithOffset(Name.size())),
-          Hint);
-
-  // Construct the diagnostic
-  DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
-  unsigned DiagID = DiagEngine.getCustomDiagID(
-          DiagnosticsEngine::Warning,
-          "Type and variable names should start with upper-case letter");
-  DiagEngine.Report(Decl->getLocation(), DiagID) << FixItHint;
 }
