6fde030d9f63945a476fabc6b894adee8dec8af6
prgrmz07 <prgrmz07@163.com>
2023-07-20 14:52:46 +0800

    插入#include正常

--- include/CodeStyleChecker/CodeStyleCheckerASTConsumer.h
+++ include/CodeStyleChecker/CodeStyleCheckerASTConsumer.h
@@ -18,19 +18,37 @@ public:
             : Visitor(R, Context),
             SM(SM)  {}
 
+    void insertInclude(clang::ASTContext &Context, clang::Rewriter& rewriter)   {
+      clang::SourceManager &SM = Context.getSourceManager();
+      clang::FileID MainFileID = SM.getMainFileID();
+      clang::SourceLocation Loc = SM.getLocForStartOfFile(MainFileID);
+
+      // Get the rewritten buffer for the main file
+      const clang::RewriteBuffer *RewriteBuf = rewriter.getRewriteBufferFor(MainFileID);
+      if (!RewriteBuf)
+        return;
+
+      // Find the location to insert the include directive
+//      unsigned Offset = Lexer::MeasureIndentation(SM.getBufferData(MainFileID), Loc, SM);
+      std::string IncludeCode = "#include <t_clock_tick.h>\n";
+
+      // Insert the include directive
+      rewriter.InsertText(Loc, IncludeCode, true, true);
+    }
+
     virtual void HandleTranslationUnit(clang::ASTContext &Ctx) override{
-        clang::TranslationUnitDecl* translationUnitDecl=Ctx.getTranslationUnitDecl();
+      clang::TranslationUnitDecl* translationUnitDecl=Ctx.getTranslationUnitDecl();
+      Visitor.TraverseDecl(translationUnitDecl);
 
-       //尝试在头部插入#include语句
-        Visitor.mRewriter.InsertTextBefore(translationUnitDecl->getBeginLoc(),"#include t_clock_tick.h");
-
-        Visitor.TraverseDecl(translationUnitDecl);
+      insertInclude(Ctx, Visitor.mRewriter);
 
 //      Visitor.mRewriter.getEditBuffer(SM.getMainFileID())
 //              .write(llvm::outs());
 
         //不在这里写出修改，而是到 函数 EndSourceFileAction 中去 写出修改
-//      Visitor.mRewriter.overwriteChangedFiles();//修改会影响原始文件
+      Visitor.mRewriter.overwriteChangedFiles();//修改会影响原始文件
+
+
     }
 
 private:
