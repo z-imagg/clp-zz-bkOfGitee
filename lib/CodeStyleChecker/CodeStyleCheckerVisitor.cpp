73355d2c91161131eb0b8bb98b9c55f6ed428636
prgrmz07 <prgrmz07@163.com>
2023-07-21 11:07:17 +0800

    内部 不可扩展 的 语法节点 内 是不能插入更多语法结构的 否则语法错误; 测试文件

--- lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
@@ -66,16 +66,29 @@ void CodeStyleCheckerVisitor::insertIncludeToFileStart(FileID fileId, clang::Sou
 
 static auto _whileStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::WhileStmt>();
 static auto _forStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::ForStmt>();
+static auto _returnStmtAstNodeKind=ASTNodeKind::getFromNodeKind<clang::ReturnStmt>();
+static auto _unaryOperatorAstNodeKind=ASTNodeKind::getFromNodeKind<clang::UnaryOperator>();
+static auto _implicitCaseExprAstNodeKind=ASTNodeKind::getFromNodeKind<clang::ImplicitCastExpr>();
 
 bool shouldInsert(clang::Stmt *S,ASTNodeKind& parent0NodeKind){
 //clang::Stmt::StmtClass & stmtClass
   clang::Stmt::StmtClass stmtClass = S->getStmtClass();
 
+  ////{
+  //{内部 不可扩展 的 语法节点 内 是不能插入更多语法结构的 否则语法错误
   //无大括号循环内语句前不要插入， 若要插入，需要先加大括号。
-  if(parent0NodeKind.isSame(_forStmtAstNodeKind) || parent0NodeKind.isSame(_whileStmtAstNodeKind)){
+  if(parent0NodeKind.isSame(_forStmtAstNodeKind) || parent0NodeKind.isSame(_whileStmtAstNodeKind)
+  //return语句内的语句前不要插入，否则语法错误。
+  ||parent0NodeKind.isSame(_returnStmtAstNodeKind)
+  //非操作符内的语句前不要插入，否则语法错误。
+  ||parent0NodeKind.isSame(_unaryOperatorAstNodeKind)
+  //隐式类型转换内的语句前不要插入，否则语法错误。
+  ||parent0NodeKind.isSame(_implicitCaseExprAstNodeKind)
+  ){
     //如果当前语句S的父亲节点是for语句头，则不插入时钟语句. 单行for语句包含 语句S， 语句S前肯定不能插入，否则 语义不对 甚至 可能语法错误 比如 变量没声明。
     return false;
   }
+  ////}
 
   switch (stmtClass) {//switch开始
     //{不插入时钟语句概率大的情况
@@ -278,10 +291,18 @@ bool CodeStyleCheckerVisitor::VisitStmt(clang::Stmt *stmt){
   const char* stmtClassName = stmt->getStmtClassName();
 
 
-//  std::cout << "[#" << stmtSourceText << "#]:{#" << stmtClassName << "#}" ;  //开发用打印
+  std::cout << "[#" << stmtSourceText << "#]:{#" << stmtClassName << "#}" ;  //开发用打印
+
+  //{开发用，条件断点
+//  bool shouldBreakPointer=stmtSourceText=="f111(";
+//  bool shouldBreakPointer2=stmtSourceText=="!f111(";
+  //}
 
   clang::DynTypedNodeList parentS=this->Ctx->getParents(*stmt);
   size_t parentSSize=parentS.size();
+  if(parentSSize>1){
+    std::cout << "注意:父节点个数大约1, 为:" << parentSSize << ",语句是:" << stmtSourceText << std::endl;
+  }
   if(parentSSize<=0){
     return true;
   }
