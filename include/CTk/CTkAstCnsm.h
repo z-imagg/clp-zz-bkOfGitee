a4faaec567e5c5ce699ade67fe21ec9616e2c54b
prgrmz07 <prgrmz07@163.com>
2023-07-22 00:17:15 +0800

    删除无用注释；整理步骤注释

--- include/CTk/CTkAstCnsm.h
+++ include/CTk/CTkAstCnsm.h
@@ -31,24 +31,12 @@ public:
 
     virtual void HandleTranslationUnit(ASTContext &Ctx) override{
 
-      LangOptions &langOptions = CI.getLangOpts();
-      //作为clang插件运行时， HandleTranslationUnit 被调用一次.
-      //独立运行时，         HandleTranslationUnit 被调用两次.
-      
-//      TranslationUnitKind Kind = CI.getFrontendOpts().ProgramAction;
-//      frontend::ActionKind actionKind = CI.getFrontendOpts().ProgramAction;
       FrontendOptions &frontendOptions = CI.getFrontendOpts();
       std::cout << "frontendOptions.ProgramAction:" << frontendOptions.ProgramAction << std::endl;
-      //作为clang插件运行时, frontendOptions.ProgramAction 值是 13:EmitObj  ;
-      //独立运行时, frontendOptions.ProgramAction 值是 25:ParseSyntaxOnly  ;
       std::cout << "Ctx.TUKind:" << Ctx.TUKind << std::endl;
-      //作为clang插件运行时、 独立运行时 , Ctx.TUKind 值是 0: TU_Complete  ;
-
-      //作为clang插件运行时, 两次调用 HandleTranslationUnit  ,  frontendOptions.ProgramAction  的两次值相同，  Ctx.TUKind 的两次值相同。
 
       FileID mainFileId = SM.getMainFileID();
-      const LangOptions & langOpts = Visitor.mRewriter.getLangOpts();
-
+//////////////////1. 若已插入 ，则不用处理
       //时钟函数只插入一次，不重复插入：
       //若已经有时钟函数调用，则标记为已处理，且直接返回，不做任何处理。
       {
@@ -68,7 +56,7 @@ public:
       }
       }
 
-
+//////////////////2. 插入时钟语句
 
       auto filePath=SM.getFileEntryForID(mainFileId)->getName().str();
       std::cout<<"处理编译单元,文件路径:"<<filePath<< ",mainFileId:" << mainFileId.getHashValue() << std::endl;
@@ -102,11 +90,13 @@ public:
         //直到第一次调用过 Visitor.TraverseDecl(Decl) 之后， Visitor.mRewriter.getRewriteBufferFor(mainFileId) 才不为NULL， 才可以用 Visitor.mRewriter 做插入动作？这是为何？
       }
       //}
-
+//////////////////3.插入包含语句
 
 
       CTkVst::insertIncludeToFileStart(mainFileId, SM, Visitor.mRewriter);//此时  Visitor.mRewriter.getRewriteBufferFor(mainFileId)  != NULL， 可以做插入
       std::cout<< "插入'包含时钟'语句到文件头部:" << filePath << ",mainFileId:" << mainFileId.getHashValue() << std::endl;
+
+//////////////////4.应用修改到源文件
 
         //不在这里写出修改，而是到 函数 EndSourceFileAction 中去 写出修改
       Visitor.mRewriter.overwriteChangedFiles();//修改会影响原始文件
