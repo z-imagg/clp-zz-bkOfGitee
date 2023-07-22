b93401979f38bbbd2afde556546db0ac9e78749a
prgrmz07 <prgrmz07@163.com>
2023-07-22 18:55:41 +0800

    测试材料

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -3,11 +3,11 @@ namespace myNs1{
 namespace myNs2{
 
 struct User{
-int f111(){
-return 11;
+int func1(){
+return 1;//func1: 在结构体定义处实现函数体
 }
-int f222(){
-return  f111(); 
+int func2(){
+return  func1();//func2: 在结构体定义处实现函数体
 }
 
 int funOutImpl();
@@ -18,13 +18,13 @@ int funOutImpl();
 }
 
 int myNs1::myNs2::User::funOutImpl() {
-  char ch;
+	char ch;//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
   ch++;
   return ch;
 }
 
 
 char topOutFunc(float f1, double d2){
-  char c=f1>0 && d2<0?'a':'b';
+	char c=f1>0 && d2<0?'a':'b';//无命名空间 的 顶层函数实现.
   return c;
 }
