cfe32af8d218b168268aedd9ef6e6d2480684ece
prgrmz07 <prgrmz07@163.com>
2023-07-22 23:17:44 +0800

    测试材料：CompoundStmt举例

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -18,13 +18,26 @@ int funOutImpl();
 }
 
 int myNs1::myNs2::User::funOutImpl() {
-	char ch;//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
+	char ch,sex='m';//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
   ch++;
   return ch;
 }
 
 
 char topOutFunc(float f1, double d2){
+  if(f1<d2){
+    int age;//CompoundStmt举例: 第1层CompoundStmt
+    float xx;
+    xx=age*10;
+    {
+      char tv;//CompoundStmt举例: 第2层CompoundStmt
+      if(tv=='b'){
+        tv*=4;
+        return tv+4;//CompoundStmt举例: 第3层CompoundStmt
+      }
+    }
+    return 'x';
+  }
   int k;
 	char c=f1>0 && d2<0?'a':'b';//无命名空间 的 顶层函数实现.
   return c;
