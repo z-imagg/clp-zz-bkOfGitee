e2c1a57b2a2bf9f6c4ec1028611e21500551a950
prgrmz07 <prgrmz07@163.com>
2023-07-22 18:22:50 +0800

    试图在这里处理 不在命名空间中的 顶层c++方法体、c函数体 ： 但没成功

--- include/CTk/CTkAstCnsm.h
+++ include/CTk/CTkAstCnsm.h
@@ -14,6 +14,36 @@
 using namespace llvm;
 using namespace clang;
 
+class Zzz{
+public:
+    static void zzz(CTkVst& worker,Decl *Child) {
+      const char *chKN = Child->getDeclKindName();
+      Decl::Kind chK = Child->getKind();
+
+      if (CXXRecordDecl *RD = dyn_cast<CXXRecordDecl>(Child)) {
+        for (CXXMethodDecl *MD : RD->methods()) {
+          Stmt *Body = MD->getBody();
+          worker.TraverseStmt(Body);
+        }
+      }
+
+      if (FunctionDecl *FD = dyn_cast<FunctionDecl>(Child)) {
+        Stmt *Body = FD->getBody();
+        Util::printStmt(*worker.Ctx, worker.CI, "上层临时查看顶层函数", "", Body, true);
+//        worker.TraverseStmt(Body);
+        worker.TraverseDecl(FD);
+      }
+      if (CXXMethodDecl *MD = dyn_cast<CXXMethodDecl>(Child)) {
+        Stmt *Body = MD->getBody();
+        Util::printStmt(*worker.Ctx, worker.CI, "上层临时查看c++方法", "", Body, true);
+//        worker.TraverseStmt(Body);
+        worker.TraverseDecl(MD);
+//        worker.TraverseDecl(Body);
+      }
+    }
+};
+
+
 class NameSpaceVst : public RecursiveASTVisitor<NameSpaceVst> {
 public:
     explicit NameSpaceVst(const CTkVst &cTkVst) : worker(cTkVst) {
@@ -29,15 +59,7 @@ public:
         const char *chKN = Child->getDeclKindName();
         Decl::Kind chK = Child->getKind();
 
-        if (CXXRecordDecl *RD = dyn_cast<CXXRecordDecl>(Child)) {
-          for (CXXMethodDecl *MD : RD->methods()) {
-            worker.TraverseStmt(MD->getBody());
-          }
-        }
-
-        if (FunctionDecl *FD = dyn_cast<FunctionDecl>(Child)) {
-          worker.TraverseStmt(FD->getBody());
-        }
+        Zzz::zzz(worker,Child);
 
       }
       ///////
@@ -80,10 +102,16 @@ public:
 
 
       for (Decl *D : DG) {
+        const char *KN = D->getDeclKindName();
+        Decl::Kind K = D->getKind();
         if (NamespaceDecl *ND = dyn_cast<NamespaceDecl>(D)) {
           NameSpaceVst namespaceVisitor(insertVst);
           namespaceVisitor.TraverseDecl(ND);
+        }else{
+          //试图在这里处理 不在命名空间中的 顶层c++方法体、c函数体 ： 但没成功
+          Zzz::zzz(insertVst,D);
         }
+
       }
       return true;
     }
