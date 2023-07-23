92e91ca715e75cf31abd3fe01e43415874a229ab
prgrmz07 <prgrmz07@163.com>
2023-07-23 17:25:17 +0800

    dev

--- tools/CTkMain.cpp
+++ tools/CTkMain.cpp
@@ -4,6 +4,7 @@
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/ArgumentsAdjusters.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Lex/PreprocessorOptions.h"
 
@@ -33,59 +34,6 @@ public:
     ASTContext& astContext=CI.getASTContext();
     //Rewriter:2:  Rewriter构造完，在Action.CreateASTConsumer方法中 调用mRewriter.setSourceMgr后即可正常使用
     mRewriter.setSourceMgr(SM, langOpts);
-
-//    CI.getPreprocessorOpts().addMacroDef("__internal_isystem=/usr/xxx/include");
-
-/*
-/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang -v -c /pubx/clang-ctk/t_clock_tick/test_main.cpp
-clang version 15.0.0 (git@github.com:llvm/llvm-project.git 4ba6a9c9f65bbc8bd06e3652cb20fd4dfc846137)
-Target: x86_64-unknown-linux-gnu
-Thread model: posix
-InstalledDir: /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin
-Found candidate GCC installation: /usr/lib/gcc/x86_64-linux-gnu/11
-Selected GCC installation: /usr/lib/gcc/x86_64-linux-gnu/11
-Candidate multilib: .;@m64
-Candidate multilib: 32;@m32
-Candidate multilib: x32;@mx32
-Selected multilib: .;@m64
- (in-process)
- "/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang-15" -cc1 -triple x86_64-unknown-linux-gnu -emit-obj -mrelax-all --mrelax-relocations -disable-free -clear-ast-before-backend -disable-llvm-verifier -discard-value-names -main-file-name test_main.cpp -mrelocation-model pic -pic-level 2 -pic-is-pie -mframe-pointer=all -fmath-errno -ffp-contract=on -fno-rounding-math -mconstructor-aliases -funwind-tables=2 -target-cpu x86-64 -tune-cpu generic -mllvm -treat-scalable-fixed-error-as-warning -debugger-tuning=gdb -v -fcoverage-compilation-dir=/ -resource-dir /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0 -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11 -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/x86_64-linux-gnu/c++/11 -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11/backward -internal-isystem /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include -internal-isystem /usr/local/include -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/11/../../../../x86_64-linux-gnu/include -internal-externc-isystem /usr/include/x86_64-linux-gnu -internal-externc-isystem /include -internal-externc-isystem /usr/include -fdeprecated-macro -fdebug-compilation-dir=/ -ferror-limit 19 -fgnuc-version=4.2.1 -fcxx-exceptions -fexceptions -fcolor-diagnostics -faddrsig -D__GCC_HAVE_DWARF2_CFI_ASM=1 -o test_main.o -x c++ /pubx/clang-ctk/t_clock_tick/test_main.cpp
-clang -cc1 version 15.0.0 based upon LLVM 15.0.0 default target x86_64-unknown-linux-gnu
-ignoring nonexistent directory "/usr/lib/gcc/x86_64-linux-gnu/11/../../../../x86_64-linux-gnu/include"
-ignoring nonexistent directory "/include"
-#include "..." search starts here:
-#include <...> search starts here:
- /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11
- /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/x86_64-linux-gnu/c++/11
- /usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11/backward
- /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include
- /usr/local/include
- /usr/include/x86_64-linux-gnu
- /usr/include
-
- */
-//    CI.getFrontendOpts().Verbosity = true;
-//    CI.getDiagnosticOpts().setVerbose(true);
-
-//    CI.getInvocation().getDiagnosticOpts().setDiagnosticOption( clang::DiagnosticIDs::DriverMode, "-verbose");
-    CI.getDiagnosticOpts().ShowColors = true;
-
-
-
-//    clang::PreprocessorOptions& preprocessorOptions=CI.getPreprocessorOpts();
-//    preprocessorOptions.addMacroDef("__internal_isystem=/usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11");
-//    preprocessorOptions.addMacroDef("__internal_isystem=/usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/x86_64-linux-gnu/c++/11");
-//    preprocessorOptions.addMacroDef("__internal_isystem=/usr/lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11/backward");
-//    preprocessorOptions.addMacroDef("__internal_isystem=/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include");
-//    preprocessorOptions.addMacroDef("__internal_isystem=/usr/local/include");
-//    preprocessorOptions.addMacroDef("__internal_isystem=/usr/lib/gcc/x86_64-linux-gnu/11/../../../../x86_64-linux-gnu/include");
-//
-////    CI.getInvocation().getLangOpts()->InternalExternCIsystem = "/zzz";
-////    CI.getInvocation().getLangOpts()->ExternCIsystem = "/zzz";
-//    CI.getPreprocessorOpts().addMacroDef("__internal_externc_isystem=/usr/include/x86_64-linux-gnu");
-//    CI.getPreprocessorOpts().addMacroDef("__internal_externc_isystem=/usr/include/x86_64-linux-gnu");
-//    CI.getPreprocessorOpts().addMacroDef("__internal_externc_isystem=/include");
-//    CI.getPreprocessorOpts().addMacroDef("__internal_externc_isystem=/usr/include");
 
 
 
@@ -137,6 +85,7 @@ int main(int Argc, const char **Argv) {
   // 设置语言选项
   CI.getLangOpts().CPlusPlus = true;
 
+
   // 设置文件名
   const char* FileName = "/pubx/clang-ctk/t_clock_tick/test_main.cpp";
   clang::FileID MainFileID = CI.getSourceManager().getOrCreateFileID(
@@ -151,8 +100,23 @@ int main(int Argc, const char **Argv) {
   clang::tooling::ClangTool Tool(Compilations, Files);//报错： No member named 'getCompilations' in 'clang::CompilerInstance'
 
 
-  Tool.appendArgumentsAdjuster(
-          clang::tooling::getInsertArgumentAdjuster("-I/usr/lib/gcc/x86_64-linux-gnu/11/include"));
+  CI.getDiagnosticOpts().ShowColors = true;
+
+  tooling::getInsertArgumentAdjuster({"-isystem", "/usr/include/c++/12"},tooling::ArgumentInsertPosition::END);
+
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-v"));
+//  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-I/usr/lib/gcc/x86_64-linux-gnu/11/include"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-resource-dir  /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include/"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /usr/include/c++/12"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /usr/include/x86_64-linux-gnu/c++/12"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /usr/include/c++/12/backward"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /usr/local/include"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-isystem  /usr/x86_64-linux-gnu/include"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-externc-isystem  /usr/include/x86_64-linux-gnu"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-externc-isystem  /include"));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("-internal-externc-isystem  /usr/include"));
   //为解决找不到 stddef.h 的初步解决办法
 
   // 运行 ClangTool
