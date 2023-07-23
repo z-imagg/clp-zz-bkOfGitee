f849b0711c7d2b98bb7c1e89638e27806de79052
prgrmz07 <prgrmz07@163.com>
2023-07-23 11:27:39 +0800

    材料还原

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,11 +1,9 @@
-#include "t_clock_tick.h"
 
 namespace myNs1{
 namespace myNs2{
 
 struct User{
 constexpr int func1(){
-X__t_clock_tick(0, 0, 0, 0);
 return 1;//func1: 在结构体定义处实现函数体
 }
 int func2(){
@@ -20,28 +18,20 @@ int funOutImpl();
 }
 
 int myNs1::myNs2::User::funOutImpl() {
-	X__t_clock_tick(0, 0, 0, 0);
-char ch,sex='m';//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
+	char ch,sex='m';//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
   ch++;
-  X__t_clock_tick(0, 0, 0, 0);
-X__t_clock_tick(0, 1, 0, 0);
-return ch;
+  return ch;
 }
 
 
 char topOutFunc(float f1, double d2){
-  {//第1层组合语句块 插入时钟 结果正确
-    X__t_clock_tick(0, 0, 0, 0);
-double sum;
-    {//第2层次组合语句块 插入时钟 结果正确
-      X__t_clock_tick(0, 0, 0, 0);
-short fix;
-    X__t_clock_tick(0, 1, 0, 0);
-}
-  X__t_clock_tick(0, 1, 0, 0);
-}
-  X__t_clock_tick(0, 0, 0, 0);
-if(f1<d2){
+  {
+    double sum;
+    {
+      short fix;
+    }
+  }
+  if(f1<d2){
     int age;//CompoundStmt举例: 第1层CompoundStmt
     float xx;
     xx=age*10;
@@ -54,11 +44,7 @@ if(f1<d2){
     }
     return 'x';
   }
-  X__t_clock_tick(0, 0, 0, 0);
-int k;
-	X__t_clock_tick(0, 0, 0, 0);
-char c=f1>0 && d2<0?'a':'b';//无命名空间 的 顶层函数实现.
-  X__t_clock_tick(0, 0, 0, 0);
-X__t_clock_tick(0, 2, 0, 0);
-return c;
+  int k;
+	char c=f1>0 && d2<0?'a':'b';//无命名空间 的 顶层函数实现.
+  return c;
 }
