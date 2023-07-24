8fe3356df2c7372e5bda426ecb8f72fcc87f6d77
prgrmz07 <prgrmz07@163.com>
2023-07-25 00:59:59 +0800

    单独else材料

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -3,9 +3,19 @@
 char topOutFunc(float f1, double d2){
   int diff;
   int arr[]={8,0,-1,99};
-  for(int q: arr)
-    if (q>0)
-      if(diff>9)
+//  for(int q: arr)
+    for(int q=0; q< 8 ; q++)
+    if (q>0)//if1
+      if(diff>9)//if2
       diff+=q;
+      else//else1
+        if (q+diff>q*diff)//if3
+        arr[q]++;
+        else//else2
+        if(false){//if4
+          arr[0]-=diff;
+        }else//else3
+          arr[diff]*=7;
+
   
 }
