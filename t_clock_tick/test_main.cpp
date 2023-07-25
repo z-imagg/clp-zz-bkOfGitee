81aca110686e97965ae13fef26d39bd53214f843
prgrmz07 <prgrmz07@163.com>
2023-07-25 22:15:16 +0800

    测试材料 ： [[gnu::fallthrough]] 作为宏定义

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,9 +1,12 @@
+#define LLVM_FALLTHROUGH [[gnu::fallthrough]]
+
 void f1(){
   int s;
   switch (s) {
     case 1:
     {
-      [[gnu::fallthrough]];
+      int age;
+      LLVM_FALLTHROUGH;
     }
     case 2:
       break;
