4c0822119a5cf4ccc7bb9805db8ef4608c8da1fd
prgrmz07 <prgrmz07@163.com>
2023-07-26 18:22:39 +0800

    CTkTest

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,31 +1,3 @@
-
-#define LLVM_FALLTHROUGH [[gnu::fallthrough]]
-
-char topOutFunc000(float f1, double d2){
-  char ch;
-  int val;
-  switch(val){
-    case 1:{
-      break;
-    }
-    case2:
-      val++;
-    case 3: {
-      ch+=val;
-    --ch;  
-    LLVM_FALLTHROUGH; //本fallthrough后面不能插入，注意本fallthrough此时并不在块尾
-  case 4:
-    
-
-    if (val>9)
-      break;
-
-    int x,  y, z=1;
-    y+=z;
-    break;
-    }
-    
-  }
+int main(int argc, char** argv){
+  return 0;
 }
-
- 
