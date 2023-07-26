6cd62849fb8b535d33d31ddde2d34e0e2cd593be
prgrmz07 <prgrmz07@163.com>
2023-07-26 23:12:17 +0800

    还原材料

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,4 +1,3 @@
-#include "t_clock_tick.h"
 #include <string>
 #include <vector>
 #include <thread>
@@ -10,8 +9,7 @@ using namespace std;
 
 void GetCurrentThreadIdAsString(std::string& curThreadIdStr){
   // 获取当前线程的ID
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::thread::id curThreadId = std::this_thread::get_id();
+  std::thread::id curThreadId = std::this_thread::get_id();
   // 线程ID转换为整数类型, 貌似依赖平台, 因此不转整形
 //  std::uintptr_t threadIdInt = reinterpret_cast<std::uintptr_t>(curThreadId);
 
@@ -20,101 +18,62 @@ std::thread::id curThreadId = std::this_thread::get_id();
 //  std::string curThreadIdStr = std::to_string(curThreadId); //只在c++11中有 std::to_string ?
 
   // 将线程的唯一标识转换为字符串类型.  不同平台, 线程id转成的字符串样式不同.
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::ostringstream outStrStream;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-outStrStream << curThreadId;
+  std::ostringstream outStrStream;
+  outStrStream << curThreadId;
 //  std::string curThreadIdStr = outStrStream.str();
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-curThreadIdStr = outStrStream.str();
+  curThreadIdStr = outStrStream.str();
 
-X__t_clock_tick(/*栈生*/0, /*栈死*/2, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
 }
 
 void thread1_func(int arg1, char & arg2){
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::string thread1IdStr;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-GetCurrentThreadIdAsString(thread1IdStr);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-printf("thread1 线程id:%s\n", thread1IdStr.c_str());
+  std::string thread1IdStr;
+  GetCurrentThreadIdAsString(thread1IdStr);
+  printf("thread1 线程id:%s\n", thread1IdStr.c_str());
 
-  X__t_clock_tick(/*栈生*/2, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-float sum,max=0;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-sum=(float)(arg1+arg2);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-max=max>sum?max:sum;
+  float sum,max=0;
+  sum=(float)(arg1+arg2);
+  max=max>sum?max:sum;
 
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-bool end;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-end=sum<max;
+  bool end;
+  end=sum<max;
 
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-printf("threa1_func:arg1:%d,arg2:%d,sum:%f,max:%f,end:%d\n",arg1,arg2,sum,max,end);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-X__t_clock_tick(/*栈生*/0, /*栈死*/4, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-return;
+  printf("threa1_func:arg1:%d,arg2:%d,sum:%f,max:%f,end:%d\n",arg1,arg2,sum,max,end);
+  return;
 }
 
 void thread2_func(int k, vector<int> arg2){
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::string thread2IdStr;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-GetCurrentThreadIdAsString(thread2IdStr);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-printf("thread2 线程id:%s\n",  thread2IdStr.c_str());
+  std::string thread2IdStr;
+  GetCurrentThreadIdAsString(thread2IdStr);
+  printf("thread2 线程id:%s\n",  thread2IdStr.c_str());
 
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-int size=arg2.size();
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-arg2[k]=size;
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-int sum=0;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-for(int z=0; z < size; z++){
-    X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-sum+=arg2[z];
+  int size=arg2.size();
+  arg2[k]=size;
+  int sum=0;
+  for(int z=0; z < size; z++){
+    sum+=arg2[z];
   }
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-printf("threa2_func:sum:%d\n",sum);
+  printf("threa2_func:sum:%d\n",sum);
 
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-X__t_clock_tick(/*栈生*/0, /*栈死*/3, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-return;
+  return;
 }
 int main(int argc, char** argv){
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::string mainThreadIdStr;
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-GetCurrentThreadIdAsString(mainThreadIdStr);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-printf("主线程 线程id:%s\n", mainThreadIdStr.c_str());
+  std::string mainThreadIdStr;
+  GetCurrentThreadIdAsString(mainThreadIdStr);
+  printf("主线程 线程id:%s\n", mainThreadIdStr.c_str());
 
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-int arg1=9;
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-char arg2='A';
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-thread thread1(thread1_func,arg1, ref(arg2));
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-thread1.detach();
+  int arg1=9;
+  char arg2='A';
+  thread thread1(thread1_func,arg1, ref(arg2));
+  thread1.detach();
 
 
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-vector<int> intS={5,-1,9,88,354,0,22};
-  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-thread thread2(thread2_func,4, intS);
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-thread2.detach();
+  vector<int> intS={5,-1,9,88,354,0,22};
+  thread thread2(thread2_func,4, intS);
+  thread2.detach();
 
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-std::this_thread::sleep_for(2s);
+  std::this_thread::sleep_for(2s);
 
-  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-X__t_clock_tick(/*栈生*/0, /*栈死*/6, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
-return 0;
+  return 0;
 }
 /**
 主线程 线程id:140658717438976
