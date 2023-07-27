0179d2e5511ce7b41372ea80bb0d3127cd8cd6d8
prgrmz07 <prgrmz07@163.com>
2023-07-27 22:54:50 +0800

    检查滴答语句平衡性检查结果

--- check_tick_balance/main.py
+++ check_tick_balance/main.py
@@ -2,7 +2,7 @@
 import pathlib
 from pathlib import Path
 import re
-from typing import List
+from typing import List, Tuple
 
 
 class File:
@@ -20,13 +20,30 @@ class File:
         self.堆生+=变量个数
     def 堆释放(self, 变量个数):
         self.堆死+=变量个数
-    def 平衡性(self)->bool:
+    def 栈平衡性(self)->bool:
         栈平衡:bool = self.栈生==self.栈死
+        return 栈平衡
+    def 堆平衡性(self)->bool:
         堆平衡:bool = self.堆生==self.堆死
+        return 堆平衡
+    def 平衡性(self)->bool:
+        栈平衡:bool =self.栈平衡性()
+        堆平衡:bool =self.堆平衡性()
         return 栈平衡 and 堆平衡
 
+    def 失衡方向(self)->Tuple[bool,bool,str,str]:
+        栈平衡:bool =self.栈平衡性()
+        堆平衡:bool =self.堆平衡性()
+
+        栈生小于死吗:bool = self.栈生<self.栈死
+        堆生小于死吗:bool = self.堆生<self.堆死
+        栈失衡方向描述:str="" if 栈平衡 else  ("栈死多" if 栈生小于死吗 else "栈生多")
+        堆失衡方向描述:str="" if 堆平衡 else  ( "堆死多" if 堆生小于死吗 else "堆生多")
+        return (栈生小于死吗,堆生小于死吗,栈失衡方向描述,堆失衡方向描述)
+
     def __str__(self):
-        return f"{self.file_path},栈:{self.栈生},{self.栈死};堆:{self.堆生},{self.堆死};"
+        _,_,栈失衡方向描述,堆失衡方向描述=self.失衡方向()
+        return f"{self.file_path},栈[{栈失衡方向描述},生:{self.栈生},死:{self.栈死}];堆:[{堆失衡方向描述},生:{self.堆生},死:{self.堆死}];"
 import glob
 import regex as re
 #  `regex`库：`regex`库是一个功能更强大且兼容性更好的正则表达式库，它提供了对PCRE（Perl兼容正则表达式）的支持。与标准库中的`re`模块相比，`regex`库提供了更多的功能和选项，例如更复杂的模式匹配、更多的元字符和修饰符等。您可以使用`pip install regex`命令安装该库。
@@ -64,18 +81,37 @@ def process_files(directory):
         if not fK.平衡性():
             print(f"失衡:{fK}")
         else:
-            print(f"平衡:{fK}")
+            # print(f"平衡:{fK}")
             pass
 ###############
 
 
 
 # 使用示例
-# directory = '/path/to/directory'  # 替换为您要递归遍历的目录路径
-# process_files(directory)
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     process_files("/pubx/llvm-project/llvm/")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+
+"""20230727 22:55 llvm添加的滴答语句 平衡检查结果:
+失衡:/pubx/llvm-project/llvm/lib/Support/BLAKE3/blake3.c,栈[栈死多,生:53,死:56];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/tools/llvm-rc/ResourceScriptParser.cpp,栈[栈死多,生:49,死:113];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/CodeGen/RegisterCoalescer.cpp,栈[栈生多,生:362,死:361];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/DebugInfo/Symbolize/MarkupFilter.cpp,栈[栈死多,生:19,死:33];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/ProfileData/InstrProfReader.cpp,栈[栈死多,生:106,死:109];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/IR/DebugInfoMetadata.cpp,栈[栈生多,生:95,死:70];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Object/WindowsResource.cpp,栈[栈死多,生:81,死:93];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Bitcode/Writer/BitcodeWriter.cpp,栈[栈死多,生:342,死:373];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Support/UnicodeNameToCodepoint.cpp,栈[栈死多,生:81,死:82];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Support/BuryPointer.cpp,栈[栈死多,生:3,死:4];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/AsmParser/LLParser.cpp,栈[栈死多,生:795,死:993];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Target/X86/X86ISelLowering.cpp,栈[栈死多,生:8274,死:8275];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Frontend/OpenMP/OMPIRBuilder.cpp,栈[栈死多,生:811,死:825];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Transforms/IPO/AttributorAttributes.cpp,栈[栈死多,生:1016,死:1019];堆:[,生:0,死:0];
+失衡:/pubx/llvm-project/llvm/lib/Transforms/IPO/OpenMPOpt.cpp,栈[栈死多,生:569,死:616];堆:[,生:0,死:0];
+"""
+
+
+# 以下文本 贴给 https://chat.chatgptdemo.net/  ， 可以获得大致代码
 # python编程：请编写通用函数（不准使用for，只能使用map()、list()、列表解析等高级特性； 不准使用os.walk， 尽可能glob、pathlib；不准使用自带re，尽可能使用三方库re2 ），递归遍历给定目录下所有 .c、.cpp 源文件， 读取每个源文件，遍历该源文件中每行，  匹配行中的 X__t_clock_tick(/*栈生*/数A, /*栈死*/数B, /*堆生*/数C, /*堆死*/数D);   ， 若有匹配  打印 数A 、数B、 数C、 数D
+
+
