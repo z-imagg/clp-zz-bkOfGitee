9121a1c26ae0b70e00a4efde42523953205aed4d
prgrmz07 <prgrmz07@163.com>
2023-07-28 01:20:31 +0800

    材料还原

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,91 +1,185 @@
-#include <string>
-#include <vector>
-#include <thread>
-#include <stdio.h>
-#include <sstream>
 
-using namespace std;
+#define LLVM_FALLTHROUGH [[gnu::fallthrough]]
 
-
-void GetCurrentThreadIdAsString(std::string& curThreadIdStr){
-  // 获取当前线程的ID
-  std::thread::id curThreadId = std::this_thread::get_id();
-  // 线程ID转换为整数类型, 貌似依赖平台, 因此不转整形
-//  std::uintptr_t threadIdInt = reinterpret_cast<std::uintptr_t>(curThreadId);
-
-//
-  // 将线程的唯一标识转换为字符串类型.  不同平台, 线程id转成的字符串样式不同.
-//  std::string curThreadIdStr = std::to_string(curThreadId); //只在c++11中有 std::to_string ?
-
-  // 将线程的唯一标识转换为字符串类型.  不同平台, 线程id转成的字符串样式不同.
-  std::ostringstream outStrStream;
-  outStrStream << curThreadId;
-//  std::string curThreadIdStr = outStrStream.str();
-  curThreadIdStr = outStrStream.str();
-
-}
-
-void thread1_func(int arg1, char & arg2){
-  std::string thread1IdStr;
-  GetCurrentThreadIdAsString(thread1IdStr);
-  printf("thread1 线程id:%s\n", thread1IdStr.c_str());
-
-  float sum,max=0;
-  sum=(float)(arg1+arg2);
-  max=max>sum?max:sum;
-
-  bool end;
-  end=sum<max;
-
-  printf("threa1_func:arg1:%d,arg2:%d,sum:%f,max:%f,end:%d\n",arg1,arg2,sum,max,end);
-  return;
-}
-
-void thread2_func(int k, vector<int> arg2){
-  std::string thread2IdStr;
-  GetCurrentThreadIdAsString(thread2IdStr);
-  printf("thread2 线程id:%s\n",  thread2IdStr.c_str());
-
-  int size=arg2.size();
-  arg2[k]=size;
-  int sum=0;
-  for(int z=0; z < size; z++){
-    sum+=arg2[z];
+char topOutFunc000(float f1, double d2){
+  int diff;
+  int arr[]={8,0,-1,99};
+  for(int p:arr)
+  for(int j:arr){
+    int zz;
   }
-  printf("threa2_func:sum:%d\n",sum);
+  if(true)
+  for(int q: arr)
+//  for(int q=0; q< 8 ; q++)
+    if (q>0)//if1
+      if(diff>9)//if2
+        diff+=q;
+      else//else1
+      if (q+diff>q*diff)//if3
+        arr[q]++;
+      else//else2
+      if(false){//if4
+        arr[0]-=diff;
+      }else//else3
+        arr[diff]*=7;
 
-  return;
+
 }
-int main(int argc, char** argv){
-  std::string mainThreadIdStr;
-  GetCurrentThreadIdAsString(mainThreadIdStr);
-  printf("主线程 线程id:%s\n", mainThreadIdStr.c_str());
-
-  int arg1=9;
-  char arg2='A';
-  thread thread1(thread1_func,arg1, ref(arg2));
-  thread1.detach();
 
 
-  vector<int> intS={5,-1,9,88,354,0,22};
-  thread thread2(thread2_func,4, intS);
-  thread2.detach();
 
-  std::this_thread::sleep_for(2s);
+namespace myNs1{
+    namespace myNs2{
 
-  return 0;
+
+        constexpr int func0(int z){
+          int a[]={};
+          for(int i:a)
+            i=z;
+          return 90;//constexpr func0
+        }
+
+
+        class Product{
+        public:
+            Product(){
+
+            }
+            Product(int id, bool ok, char cate='Z')
+                    :id(id),ok(ok),cate(cate),owner_id(-1)
+            {
+              int thanks,memory=100;
+              while(true){
+                bool err=false;
+                if(err){
+                  break;
+                }
+              }
+            }
+            Product(Product &&Other) = default;
+            Product &operator=(Product &&Other) = default;
+
+            void switch_demo_func(){
+              int z;
+
+              switch(z)
+              {//switch身体开始
+
+                case 1://case 1
+                {
+                  int idea; float aa,bb,cc=0.9; double sex;
+                  z=z*98+z/3;//倒数第二条语句右是宏周边，位置不准。 但倒数第二条语句左不是宏周边，位置是准的。
+                  LLVM_FALLTHROUGH;//宏周边位置不准
+                }
+
+                case 2:
+                {//case 2 身体 开始
+                  bool valid;
+                  if(valid){
+                    break;
+                  }else{
+                    valid=true;
+                  }
+                }//case 2 身体 结尾
+
+                default:
+                {//switch的default 身体 开始
+                  float f;//switch's default content
+                  return;
+                }//witch的default 身体结尾
+
+              }//switch(z) 结尾
+
+            }//switch_demo_func函数 结尾
+
+        private:
+            int id;
+            bool ok;
+            char cate;
+            int owner_id;
+        };
+
+        struct User{
+            constexpr int func1(){
+              return 1;//func1: 在结构体定义处实现函数体
+            }
+            int func2(){
+              return  func1();//func2: 在结构体定义处实现函数体
+            }
+
+            int funOutImpl();
+        };
+
+    }
+
 }
-/**
-主线程 线程id:140658717438976
-thread2 线程id:140658702542400
-thread1 线程id:140658710935104
 
-c++线程id: 单进程内单时刻唯一、单进程内两异时间段可相同
- 1. 在单一进程内任意单一时刻 全部活着的线程id 唯一。
- 2. 在单一进程内,  单一线程id 可以存活在 两个不同的时间段内。
+int myNs1::myNs2::User::funOutImpl() {
+  char ch,sex='m';//命名空间内的 函数 funOutImpl: 在结构体定义外实现函数体
+  ch++;
+  while(ch!='a'){
+    int num,sum=0;
+    char flag;
+    float delta=0.9, alpha, ZERO=0.0, PI=3.14, DIFF;
+    ch--;
+    //此块末尾 应该释放8个变量, 而不是3个变量。
+  }
+  return ch;
+}
 
-//主线程 将 线程1 脱离  后， 主线程 又将 线程2 脱离。
- 即 主线程、线程1、线程2  在时间轴上 极大概率重合。  因此  三线程id 不能重复。
 
+char topOutFunc(float f1, double d2){
+  int arr[]={8,0,-1,99};
+  for(int x: arr){
+    bool ok=x%2==0;
+    if(ok) break;
+  }
 
- */
+  {
+    double sum;
+    {
+      short fix;
+    }
+  }
+  if(f1<d2){
+    int age;//CompoundStmt举例: 第1层CompoundStmt
+    float xx;
+    xx=age*10;
+    {
+      char tv;//CompoundStmt举例: 第2层CompoundStmt
+      if(tv=='b'){
+        tv*=4;
+        return tv+4;//CompoundStmt举例: 第3层CompoundStmt
+      }
+    }
+    return 'x';
+  }
+  int k;
+  char c=f1>0 && d2<0?'a':'b';//无命名空间 的 顶层函数实现.
+  return c;
+}
+
+#include <exception>
+#include <iostream>
+
+static void* staticFunc(int* arg){
+  char (*funcPtr)(int, char, short)=0;
+  try{
+    int num=*arg;
+    char ch='A';
+    short shrt=(short)num/10;
+    try{
+      char result=funcPtr(num,ch,shrt);
+      if(!result)
+        throw "err02";
+    }catch (std::exception& exception){
+      std::cout<< exception.what() << std::endl;
+    }
+  }catch (const char* errmsg){
+    std::cout<< errmsg << std::endl;
+  }catch(std::exception & e){
+    std::cout<< e.what() << std::endl;
+  }catch (...){
+    throw "other exception";
+  }
+}
