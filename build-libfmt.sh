24cd4b51fa29eeae05c37b6fe6a438b19acc38d2
prgrmz07 <prgrmz07@163.com>
2023-07-29 16:08:29 +0800

    libfmt构建参数:  " -fno-omit-frame-pointer -Wall   -O0  "

--- build-libfmt.sh
+++ build-libfmt.sh
@@ -8,10 +8,15 @@ git clone git@gitcode.net:pubx/fmtlib/fmt.git
 cd $REPO_HOME
 git checkout 10.0.0
 
+_CXX__FLAG=" -fno-omit-frame-pointer -Wall   -O0  -fPIC "
 
-rm -fr $BUILD_HOME
-mkdir $BUILD_HOME && cd $BUILD_HOME
-cmake $REPO_HOME
+
+rm -fr $BUILD_HOME && mkdir $BUILD_HOME && cd $BUILD_HOME
+cmake \
+-DFMT_TEST=OFF \
+-DCMAKE_CXX_FLAGS="$_CXX__FLAG"  -DCMAKE_C_FLAGS="$_CXX__FLAG" \
+-S $REPO_HOME
+
 make -j8
 
 ls -lh $BUILD_HOME/libfmt.a
