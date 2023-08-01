6b8e133c660d79d557f37ea05b19e8ba14b7a939
prgrmz07 <prgrmz07@163.com>
2023-08-01 09:43:31 +0800

    草稿：函数调用结构体

--- t_clock_tick/t_clock_tick.h
+++ t_clock_tick/t_clock_tick.h
@@ -2,6 +2,17 @@
 #ifndef _T_CLOCK_TICK_H
 #define _T_CLOCK_TICK_H
 
+struct _XFuncCall{
+    char * srcFile;
+    char * funcName;
+    int funcLine;
+    int funcCol;
+
+    long funcEnterId;
+
+    int topFuncSVarCnt;
+};
+typedef struct _XFuncCall XFuncCall;
 
 #ifndef __cplusplus
 //如果当前是.c文件包含了此.h,  则强制使用#函数名到c++名字
@@ -17,7 +28,7 @@
  * @param heapObjAllocCnt   此次滴答期间， 堆对象分配数目
  * @param heapObjcFreeCnt   此次滴答期间， 堆对象释放数目
  */
-void X__t_clock_tick(int dSVarAC, int dSVarFC, int dHVarAC, int dHVarFC, int* topFuncSVarCnt_ptr);
+void X__t_clock_tick(int dSVarAC, int dSVarFC, int dHVarAC, int dHVarFC, int* xFuncCall_ptr);
 
 /////函数X__funcEnter、X__funcReturn用于:  return语句 应该释放 本函数当前已经申请的所有栈变量。
 /**
@@ -27,5 +38,5 @@ void X__funcEnter( );
 /**
  * 函数X__funcReturn插入在函数的每条return语句之前，以及void函数的末尾之前
  */
-void X__funcReturn(int* topFuncSVarCnt_ptr );
+void X__funcReturn(int* xFuncCall_ptr );
 #endif //_T_CLOCK_TICK_H
