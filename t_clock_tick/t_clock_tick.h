8b663af61b238c978a91e9391a22437855cfb0bc
prgrmz07 <prgrmz07@163.com>
2023-08-01 09:39:07 +0800

    整理注释t_clock_tick.h

--- t_clock_tick/t_clock_tick.h
+++ t_clock_tick/t_clock_tick.h
@@ -13,13 +13,6 @@
 // extern int hVarFreeCnt;//当前堆对象释放数目
 // extern int hVarCnt;//当前堆对象数目（冗余）
 
-/**
- *
- * @param stackVarAllocCnt  此次滴答期间， 栈变量分配数目
- * @param stackVarFreeCnt   此次滴答期间， 栈变量释放数目
- * @param heapObjAllocCnt   此次滴答期间， 堆对象分配数目
- * @param heapObjcFreeCnt   此次滴答期间， 堆对象释放数目
- */
 #ifndef __cplusplus
 //如果当前是.c文件包含了此.h,  则强制使用#函数名到c++名字
 #define X__t_clock_tick  _Z15X__t_clock_tickiiiiPi
@@ -27,6 +20,13 @@
 #define X__funcReturn _Z13X__funcReturnPi
 #endif
 
+/**
+ *
+ * @param stackVarAllocCnt  此次滴答期间， 栈变量分配数目
+ * @param stackVarFreeCnt   此次滴答期间， 栈变量释放数目
+ * @param heapObjAllocCnt   此次滴答期间， 堆对象分配数目
+ * @param heapObjcFreeCnt   此次滴答期间， 堆对象释放数目
+ */
 void X__t_clock_tick(int dSVarAC, int dSVarFC, int dHVarAC, int dHVarFC, int* topFuncSVarCnt_ptr);
 
 /////函数X__funcEnter、X__funcReturn用于:  return语句 应该释放 本函数当前已经申请的所有栈变量。
