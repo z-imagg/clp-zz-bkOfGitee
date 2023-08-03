ce4eeb9057f393bf335e856a00cce713ad538762
prgrmz07 <prgrmz07@163.com>
2023-08-03 23:59:12 +0800

    Revert "Revert "Revert "替换为自己构建的/pubx/build-llvm15"""
    This reverts commit 7fa50b5d4951432d00014c054f1bac18b3734341.

--- clang15_plugin.md
+++ clang15_plugin.md
@@ -183,9 +183,9 @@ int main(int Argc, const char **Argv) {
 
   Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster("--verbose"));
   //解决： 找不到 stddef.h
-  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster({"-resource-dir","/pubx/build-llvm15/lib/clang/15.0.0"},tooling::ArgumentInsertPosition::END));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster({"-resource-dir","/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0"},tooling::ArgumentInsertPosition::END));
   /* stddef.h位于:
-/pubx/build-llvm15/lib/clang/15.0.0/include/stddef.h
+/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include/stddef.h
    */
 
   // 运行 ClangTool
--- toolchain.cmake
+++ toolchain.cmake
@@ -1,6 +1,6 @@
 #ref: https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/CrossCompiling
-#cmake -DCT_Clang_INSTALL_DIR=/pubx/build-llvm15/  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCURSES_LIBRARY=/lib64/libncurses.so.6 -DCURSES_INCLUDE_PATH=/usr/include/   -DCMAKE_EXPORT_COMPILE_COMMANDS=True   -DCMAKE_C_COMPILER=/pubx/build-llvm15/bin/clang -DCMAKE_CXX_COMPILER=/pubx/build-llvm15/bin/clang++    -DLLVM_DIR=/pubx/build-llvm15 ..
+#cmake -DCT_Clang_INSTALL_DIR=/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCURSES_LIBRARY=/lib64/libncurses.so.6 -DCURSES_INCLUDE_PATH=/usr/include/   -DCMAKE_EXPORT_COMPILE_COMMANDS=True   -DCMAKE_C_COMPILER=/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang -DCMAKE_CXX_COMPILER=/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang++    -DLLVM_DIR=/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4 ..
 
 #cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake ..
-SET(CMAKE_C_COMPILER   /pubx/build-llvm15/bin/clang)
-SET(CMAKE_CXX_COMPILER /pubx/build-llvm15/bin/clang++)
+SET(CMAKE_C_COMPILER   /llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang)
+SET(CMAKE_CXX_COMPILER /llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang++)
