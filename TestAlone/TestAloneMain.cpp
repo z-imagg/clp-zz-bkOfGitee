c1a0f327f112fac25ea38e055b8eca124bb5b20b
prgrmz07 <prgrmz07@163.com>
2023-08-03 21:49:16 +0800

    zz

--- TestAlone/TestAloneMain.cpp
+++ TestAlone/TestAloneMain.cpp
@@ -37,15 +37,10 @@ public:
 
 int main(int Argc, const char **Argv  ) {
 
-//  clang::tooling::CompilationDatabase &Compilations =  * new clang::tooling::FixedCompilationDatabase(Twine("."), std::vector<std::string>());
-
   clang::CompilerInstance CI;
   CI.createDiagnostics();
   CI.createFileManager();
   CI.createSourceManager(CI.getFileManager());
-/* 确保在创建编译器实例（`clang::CompilerInstance`）后，立即创建源管理器（`clang::SourceManager`）并将其设置到编译器实例中
-否则 ，运行时 断言失败 报错 ：   CompilerInstance.h:423: clang::SourceManager& clang::CompilerInstance::getSourceManager() const: Assertion `SourceMgr && "Compiler instance has no source manager!"' failed.
- */
 
   CI.getLangOpts().CPlusPlus = true;
 
@@ -58,7 +53,6 @@ int main(int Argc, const char **Argv  ) {
   TargetInfo* targetInfo=  TargetInfo::CreateTargetInfo(CI.getDiagnostics(), targetOpts) ;
   CI.setTarget(targetInfo);
 
-//  CI.head();
   CI.createPreprocessor(clang::TU_Complete);
   CI.getPreprocessor().Initialize(*targetInfo);
 
@@ -66,39 +60,18 @@ int main(int Argc, const char **Argv  ) {
 
   StringRef Code ("void func(int i, int time) { return; }");
 
-  // 创建一个MemoryBuffer对象
-//  std::unique_ptr<llvm::MemoryBuffer> Buf =
-//          llvm::MemoryBuffer::getMemBuffer(Code/*,"input.cpp"*/);
-
-//  MemoryBufferRef BufRef(*Buf);
   MemoryBufferRef BufRef(Code,"exampleCPP");
 
   SourceManager& SM=CI.getSourceManager();
-  FileManager &FM = CI.getFileManager();
-  // 获取输入文件
-//  const FileEntry *File = CI.getFileManager().getVirtualFile("input.cpp", Buf->getBufferSize(), 0);
-//  FileID MainFileID = SM.createFileID(File, SourceLocation(), SrcMgr::C_User);
-//  SM.setMainFileID(MainFileID);
 
-//  FileID MainFileID = SM.createMainFileIDForMemBuffer(std::move(Buf));
-//  SM.setMainFileID(MainFileID);
-
-  // 创建一个输入文件缓冲区
-//  clang::FileID MainFileID =
-//  SM.getOrCreateFileID( File, clang::SrcMgr::C_User);
   clang::FileID MainFileID = SM.createFileID(
           BufRef, clang::SrcMgr::C_User);
     SM.setMainFileID(MainFileID);
 
 
   Preprocessor &PP = CI.getPreprocessor();
-//  CI.createPreprocessor(clang::TU_Complete);
-  // 创建一个Lexer对象
 //  Lexer RawLexer(SM.getMainFileID(), BufRef, SM, LangOpts);
   Lexer RawLexer(SM.getMainFileID(), BufRef, PP, true);
-//  clang::Lexer lexer(CI.getPreprocessor().getLangOpts(), CI.getPreprocessor().getPreprocessorAllocator(), CI.getPreprocessor().getSourceManager(), CI.getPreprocessor().getLangOpts());
-
-//  Lexer(FileID FID, const llvm::MemoryBufferRef &InputFile, Preprocessor &PP, bool IsFirstIncludeOfFile)
 
 //  MyASTConsumer myAstConsumer;
 //  std::unique_ptr<ASTConsumer> Consumer = std::make_unique<MyASTConsumer>();
@@ -111,18 +84,12 @@ int main(int Argc, const char **Argv  ) {
   // 创建一个Parser对象
   Parser parser(CI.getPreprocessor(), seam, false);
 
-
-
-  // 解析代码
-
   parser.Initialize();
   parser.ConsumeToken();
   parser.ParseTopLevelDecl();
 
-  // 获取ASTContext
   ASTContext &Context = CI.getASTContext();
 
-  // 输出结果
   TranslationUnitDecl *TUDecl = Context.getTranslationUnitDecl();
   bool hasBody = TUDecl->hasBody();
   for (Decl *D : TUDecl->decls()) {
@@ -134,28 +101,6 @@ int main(int Argc, const char **Argv  ) {
 
 
 
-//  clang::FileID MainFileID = CI.getSourceManager().createFileID(
-//          llvm::MemoryBuffer::getMemBuffer(Code), clang::SrcMgr::C_User);
-//
-//  SourceLocation StartLoc = SM.getLocForStartOfFile(MainFileID);
-//  const std::string &StartLocStr = StartLoc.printToString(SM);
-//
-//  SourceLocation StartLoc_getLocForEndOfToken = Lexer::getLocForEndOfToken(StartLoc, 0, SM, LangOptions());
-//  const std::string &StartLoc_getLocForEndOfToken_Str = StartLoc_getLocForEndOfToken.printToString(SM);
-//
-//  SourceLocation EndLoc = SM.getLocForEndOfFile(MainFileID);
-//  const std::string &EndLocStr = EndLoc.printToString(SM);
-//
-//  SourceLocation EndLoc_getLocForEndOfToken = Lexer::getLocForEndOfToken(EndLoc, 0, SM, LangOptions());
-//  const std::string &EndLoc_getLocForEndOfToken_Str = EndLoc_getLocForEndOfToken.printToString(SM);
-//
-//  // 输出结果
-//  std::cout << "代码:"  << std::endl;
-//  std::cout <<  Code << std::endl;
-//  std::cout << "StartLocStr:" << StartLocStr << std::endl;
-//  std::cout << "getLocForEndOfToken(StartLoc,0):" << StartLoc_getLocForEndOfToken_Str << std::endl;
-//  std::cout << "EndLocStr:" << EndLocStr << std::endl;
-//  std::cout << "getLocForEndOfToken(EndLoc,0):" << EndLoc_getLocForEndOfToken_Str << std::endl;
 
   return 0;
 }
