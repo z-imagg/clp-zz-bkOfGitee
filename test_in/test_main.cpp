b7b4e6442642bc95c2d1ba855b013f6dd47e56dc
prgrmz07 <prgrmz07@163.com>
2023-08-05 11:47:31 +0800

    bug: 第二批if 即此if 的then不会被插入花括号

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -6,7 +6,7 @@ bool  nonMicrosoftDemangle(  char *MangledName, char * &Result) {
   else if (MangledName+2)
     Demangled = MangledName+3;
 
-  if (!Demangled)//bug: 此if的then不会被插入花括号
+  if (!Demangled)//bug: 第二批if 即此if 的then不会被插入花括号
     return false;
 
   Result = Demangled;
