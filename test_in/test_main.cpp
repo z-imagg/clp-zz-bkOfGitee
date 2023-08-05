05c35d1a50c103a2184e63c206a97a2965c3ccb5
prgrmz07 <prgrmz07@163.com>
2023-08-05 11:45:22 +0800

    测试材料：bug: 第2批次if的then不会被插入花括号

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,109 +1,14 @@
-#include <iostream>
-/**
- * 这是一个示例类
- */
-class MyClass {
-public:
-    static int ZERO;
-public:
-    /**
-     * 这是一个示例成员函数
-     * @param a 参数a
-     * @param b 参数b
-     * @return 返回结果
-     */
-    int myFunction(int a, int b) {
-      return a + b;
-    }
 
-  char* switchDemo(int cnt, short chr){
-    double sum=1.*cnt*chr;
-    start: char L;
-    switch(chr){
-      case 0:
-      {
-        L=(cnt+chr)%50;
-        return &L+1;
-      }
+bool  nonMicrosoftDemangle(  char *MangledName, char * &Result) {
+  char *Demangled = nullptr;
+  if (MangledName+1)
+    Demangled = MangledName+1;
+  else if (MangledName+2)
+    Demangled = MangledName+3;
 
+  if (!Demangled)//bug: 此if的then不会被插入花括号
+    return false;
 
-      case 1:
-        int unit;
-        unit=18;
-        return NULL;
-      case 2:
-        cnt=L*L;
-        cnt++;
-        break;
-      case 3:
-        return &L;
-      case 4:
-      {
-        L++;
-        sum=L+sum;
-        if (L)  goto start;
-      }
-      return (char*)0;
-      case 6:{
-        int zB;
-        break;
-      }
-      ////////
-      case 7:{
-        int uuuuu;
-        break;
-      }
-      default:
-        sum+=chr*cnt;
-        return &L;
-    }
-    return NULL;
-  }
-};
-
-int MyClass::ZERO=0;
-
-// 这是一个示例全局函数
-/**
- * @brief 这是一个示例全局函数
- * @param x 参数x
- * @param y 参数y
- * @return 返回结果
- */
-int myGlobalFunction(int x, int y) {
-  char ch;
-  if(x>y)
-    if(x<5)
-      if (y>600)
-      while(x++ + y > 12)
-        y += x  * 2  - y  + MyClass::ZERO    ;
-      else if(y>0)
-        for(int t=0; t<x+y; t+=x)
-          y = x + t - 1/t ;
-      else
-        return ch * x / y  ;
-    else
-      ch = ch * myGlobalFunction(ch, y)   ;
-
-  return x * y;
-}
-
-void func_macro_inc(){
-  double PI;
-  if(true)
-#include "Math.def.h"
-}
-int main() {
-  MyClass obj;
-  int result = obj.myFunction(10, 20);
-  int x,y,z;
-  if(true)
-  x = y + MyClass::ZERO   ;  
-  else
-  z *= x /  myGlobalFunction(z, y)   ;    
-
-  int product = myGlobalFunction(5, 6);
-  std::cout << "Product: " << product << std::endl;
-
-  return 0;
+  Result = Demangled;
+  return true;
 }
