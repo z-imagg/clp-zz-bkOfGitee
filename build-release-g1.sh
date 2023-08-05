daf11ef01981d796bb680bd89e384ad568bc2511
prgrmz07 <prgrmz07@163.com>
2023-08-05 16:45:19 +0800

    build脚本 改为只构建目标 BrcPlugin， 且能被调用

--- build-release-g1.sh
+++ build-release-g1.sh
@@ -1,4 +1,5 @@
-BUILD_HOME=/pubx/clang-brc/build/
+SRC_HOME=/pubx/clang-brc
+BUILD_HOME=$SRC_HOME/build/
 mkdir -p $BUILD_HOME
 rm -fr $BUILD_HOME/*
 cd $BUILD_HOME
@@ -15,11 +16,13 @@ LIB_Brc_CXX_FLAGS=" -fno-omit-frame-pointer -Wall   -O0    -g1"
 # LIB_Brc_CXX_FLAGS=" -fno-omit-frame-pointer -Wall   -O0    -g2"
 # -rwxrwxr-x 1 zz zz 27M  7月 24 15:03 /pubx/clang-brc/build/lib/libBrcPlugin.so
 
-cmake -DCMAKE_BUILD_TYPE=Release   -DCMAKE_CXX_FLAGS="$LIB_Brc_CXX_FLAGS"   -DCMAKE_C_FLAGS="$LIB_Brc_CXX_FLAGS"  ..
+#-S .. -B .
+cmake -DCMAKE_BUILD_TYPE=Release   -DCMAKE_CXX_FLAGS="$LIB_Brc_CXX_FLAGS"   -DCMAKE_C_FLAGS="$LIB_Brc_CXX_FLAGS"  -S $SRC_HOME  -B $BUILD_HOME
 #-DCMAKE_VERBOSE_MAKEFILE=True
-make -j8
+#make -j8
+cmake --build $BUILD_HOME  --target BrcPlugin -j 4
+#以cmake代替make来执行构建给定目标
 ls -lh $BUILD_HOME/lib/lib*.so
-
 
 
 ## 构建Release版本且有带局部变量名字的调试信息 以定位问题
