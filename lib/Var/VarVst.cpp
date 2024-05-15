c66fc581d978a85115bfb413ea660e00a4e0d500
prgrmz07 <prgrmz07@163.com>
2024-05-15 14:07:52 +0800

    [5]TraverseDeclStmt ： 打印微调（换行）

--- lib/Var/VarVst.cpp
+++ lib/Var/VarVst.cpp
@@ -34,6 +34,6 @@ bool VarVst::TraverseDeclStmt(DeclStmt* declStmt){
     //构造人类可读开始位置、结束位置、插入者 注释文本
     std::string hmTxtCmnt_whoInsrt_BE;
     Util::BE_Loc_HumanText(SM, beginLoc, endLoc, "test_VisitDeclStmt", hmTxtCmnt_whoInsrt_BE);
-    std::cout<<hmTxtCmnt_whoInsrt_BE;
+    std::cout<<hmTxtCmnt_whoInsrt_BE<<"\n";
     return true;
 }
