a73c826ff1df5e42c34c7934be32db366e3e71d9
prgrmz07 <prgrmz07@163.com>
2024-05-16 20:38:31 +0800

    从CTK复制来programa处理

--- include/Var/CollectIncMacro_PPCb.h
+++ include/Var/CollectIncMacro_PPCb.h
@@ -33,6 +33,26 @@ public:
 
     static bool hasInclusionDirective(SourceManager& SM, SourceRange range);
     static bool hasMacroDefined(SourceManager& SM, SourceRange range);
+	
+	////program
+	
+	void PragmaMessage(SourceLocation Loc, StringRef namespaceSR, PPCallbacks::PragmaMessageKind msgKind, StringRef msgSR) ;
+
+    /** pragma_message_set 是 方法PragmaMessage 被回调后的结果
+     */
+    static std::set<std::string> pragma_message_set;
+
+    /**
+     * pragmaMsgFull决定了Constant::NameSpace_funcIdAsmIns的值的样式是 "命名空间:pragmaMessgae"
+     * @param namespac
+     * @param pragmaMessage
+     * @return
+     */
+    static std::string pragmaMsgFull(std::string namespac, std::string pragmaMessage){
+        auto msgFull=fmt::format("{}:{}",namespac,pragmaMessage);
+        return msgFull;
+    }
+
 };
 
 
--- lib/Var/CollectIncMacro_PPCb.cpp
+++ lib/Var/CollectIncMacro_PPCb.cpp
@@ -91,3 +91,43 @@ using namespace clang;
 
 std::unordered_set<LocId,LocId> CollectIncMacro_PPCb::InclusionDirectiveLocSet;
 std::unordered_set<LocId,LocId> CollectIncMacro_PPCb::MacroDefinedLocSet;
+
+
+////program
+
+//如果本源文件中根本没有#pragma ，则方法PragmaMessage不会被调用
+    void CollectIncMacro_PPCb::PragmaMessage(SourceLocation Loc, StringRef namespaceSR, PPCallbacks::PragmaMessageKind msgKind, StringRef msgSR) {
+        //region 方便变量
+        SourceManager &SM = CI.getSourceManager();
+        //endregion
+
+        //region 获取主文件ID,文件路径
+        FileID mainFileId;
+        std::string filePath;
+        Util::getMainFileIDMainFilePath(SM,mainFileId,filePath);
+        //endregion
+
+        //region 跳过非主文件
+        if(!SM.isWrittenInMainFile(Loc)){
+            return;
+        }
+        //endregion
+
+        if(PPCallbacks::PragmaMessageKind::PMK_Message != msgKind){
+            return;
+        }
+
+        //region 收集  #pragma message
+        auto msg=msgSR.str();
+        auto namespac=namespaceSR.str();
+
+        auto msgFull=CollectIncMacro_PPCb::pragmaMsgFull(namespac,msg);
+
+        pragma_message_set.insert(msgFull);
+        std::cout << fmt::format("namespaceSR:{} , msgSR:{}, msgKind:{}, msgFull:{}\n", namespaceSR.str(), msgSR.str() , (int)msgKind, msgFull) ;
+
+        //endregion
+    }
+
+
+std::set<std::string> CollectIncMacro_PPCb::pragma_message_set;
