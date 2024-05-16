5b09b1ae317b8fd47e908f09487c83ade491091e
prgrmz07 <prgrmz07@163.com>
2024-05-16 23:00:47 +0800

    常量改为VarBE_inserted

--- include/Var/Constant.h
+++ include/Var/Constant.h
@@ -6,10 +6,10 @@
 
 class Constant{
 public:
-    const   std::string PrgMsgStmt_funcIdAsmIns = "#pragma message(\"funcId_asm_inserted\")\n";
+    const   std::string PrgMsgStmt_funcIdAsmIns = "#pragma message(\"VarBE_inserted\")\n";
 
     //CollectIncMacro_PPCb::pragmaMsgFull决定了Constant::NameSpace_funcIdAsmIns的值的样式是 "命名空间:pragmaMessgae"
-    const   std::string NameSpace_funcIdAsmIns = ":funcId_asm_inserted";
+    const   std::string NameSpace_funcIdAsmIns = ":VarBE_inserted";
 
 };
 #endif //Var_CONSTANT_H
