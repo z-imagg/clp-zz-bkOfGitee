b0d740ada94a045be38c7ea11184670cf87b5998
prgrmz07 <prgrmz07@163.com>
2024-05-16 13:46:16 +0800

    [5] 全草稿

--- runtime_cpp__vars_fn/test_main__runtime_cpp__vars_fn.cpp
+++ runtime_cpp__vars_fn/test_main__runtime_cpp__vars_fn.cpp
@@ -4,20 +4,20 @@ class T_User{};
 class T_Book{};
 class T_Auth{};
 
-void func1(){std::vector<_VarDecl> * _vdVec=_init_varLs_inFn("runtime_cpp__vars_fn/test_main__runtime_cpp__vars_fn.cpp","func1",5,14);
+void func1(){_VarDeclLs * _vdLs=_init_varLs_inFn("runtime_cpp__vars_fn/test_main__runtime_cpp__vars_fn.cpp","func1",7,14);
     int a, b, c, *d;
-    T_User user1, user2, *ptr_user3; createVar(_vdVec,"T_User", 2);
+    T_User user1, user2, *ptr_user3; createVar(_vdLs,"T_User", 2);
 
     if(true){
-        for(int i=0; i < 4; i++){
-            T_Book book1, book2, *ptr_book3,book4; createVar(_vdVec,"T_Book", 3);
+        for(int i=1; i <= 2; i++){
+            T_Book book1, book2, *ptr_book3,book4; createVar(_vdLs,"T_Book", 3);
             {
-                T_Auth auth1; createVar(_vdVec,"T_Auth", 1);
+                T_Auth auth1,auth2,*ptr_auth3; createVar(_vdLs,"T_Auth", 2);
             }
         }
     }
 
-destroyVarLs_inFn(_vdVec);}
+destroyVarLs_inFn(_vdLs);}
 
 
 int main(int argc, char** argv){
