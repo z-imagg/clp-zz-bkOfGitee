461b42dd0c61ab61f11f75ff5f48bf8a813d61f9
prgrmz07 <prgrmz07@163.com>
2024-05-16 13:11:16 +0800

    [3] 全草稿

--- runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
+++ runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
@@ -2,8 +2,17 @@
 #ifndef runtime_cpp__vars_fn_H
 #define runtime_cpp__vars_fn_H
 
+#include <vector>
+#include <string>
+struct __VarDecl{
+    std::string varTypeName;
+    int varCnt;
+};
+
+typedef struct __VarDecl _VarDecl;
+
 //TODO  函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
-void* _init_varLs_inFn();
+std::vector<_VarDecl> *  _init_varLs_inFn(char* srcFilePath,char* funcName,int funcLBrc_line,int funcLBrc_column);
 
 
 //结构体变量声明末尾 插入 'createVar(_varLs_ptr,"变量类型名",变量个数);'
--- runtime_cpp__vars_fn/runtime_cpp__vars_fn.cpp
+++ runtime_cpp__vars_fn/runtime_cpp__vars_fn.cpp
@@ -1,32 +1,23 @@
 
 #include "runtime_cpp__vars_fn.h"
-#include <string>
-struct _VarDecl{
-    std::string varTypeName;
-    int varCnt;
-};
 
-//typedef struct _VarDecl VarDec;
 
-#include <vector>
+
 #include <iostream>
 #include <algorithm>
 
 
-//TODO  函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
-void* _init_varLs_inFn(){
-    std::vector<struct _VarDecl> *_vdVec = new std::vector<struct _VarDecl>();
-    static_cast<void*>(_vdVec);
-//    return (void*)_vdVec;
-    return static_cast<void*>(_vdVec);
+//函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
+std::vector<_VarDecl> *  _init_varLs_inFn(char* srcFilePath,char* funcName,int funcLBrc_line,int funcLBrc_column){
+    std::vector<_VarDecl> *_vdVec = new std::vector<_VarDecl>();
+    return _vdVec;
 }
 
 
 //结构体变量声明末尾 插入 'createVar(_varLs_ptr,"变量类型名",变量个数);'
-void createVar(void* _varLs_ptr, char* varTypeName,int varCnt){
-    std::vector<struct _VarDecl> *_vdVec = static_cast<std::vector<struct _VarDecl> *>(_varLs_ptr);
+void createVar(std::vector<_VarDecl> * _vdVec, char* varTypeName,int varCnt){
 
-    struct _VarDecl vd;
+    _VarDecl vd;
     vd.varTypeName=varTypeName;
     vd.varCnt=varCnt;
 
@@ -35,14 +26,15 @@ void createVar(void* _varLs_ptr, char* varTypeName,int varCnt){
 }
 
 
-//【销毁变量通知】 TODO  函数右花括号前 插入 'destroyVarLs_inFn(_varLs_ptr);'
-void destroyVarLs_inFn(void* _varLs_ptr){
-    std::vector<struct _VarDecl> *_vdVec = static_cast<std::vector<struct _VarDecl> *>(_varLs_ptr);
+//【销毁变量通知】 函数右花括号前 插入 'destroyVarLs_inFn(_varLs_ptr);'
+void destroyVarLs_inFn(std::vector<_VarDecl> *  _vdVec){
 
     long vdCnt = std::distance(_vdVec->begin(), _vdVec->end());
     std::cout<<"varDeclCnt="<<vdCnt<<std::endl;
 
-    std::for_each(_vdVec->begin(), _vdVec->end(), [](const struct _VarDecl vd){
+    std::for_each(_vdVec->begin(), _vdVec->end(), [](const _VarDecl vd){
         std::cout<<"vd:{varTypeName="<<vd.varTypeName<<",varCnt="<<vd.varCnt<<"}"<<std::endl;
     });
+
+    delete _vdVec;
 }
