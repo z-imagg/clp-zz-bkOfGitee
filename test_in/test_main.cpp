4a60608c2e429b6154bb5595c46e2b2d6f2c89d6
prgrmz07 <prgrmz07@163.com>
2024-05-18 21:59:08 +0800

    测试材料插入varBE 、后 正常编译、运行 (varBE的destroy有正常打印）
    PrjHm=/fridaAnlzAp/clang-var/
    # 以clang编译命令-include 等效test_main.cpp中书写了'#include "runtime_cpp__vars_fn.h"'
    clang++ -I $PrjHm/runtime_cpp__vars_fn/include/ -include runtime_cpp__vars_fn.h  -c   $PrjHm/test_in/test_main.cpp
    # 插入varBE后测试材料 正常编译
    
    """
    /fridaAnlzAp/clang-var//test_in/test_main.cpp:1:9: warning: VarBE_inserted [-W#pragma-messages]
    #pragma message("VarBE_inserted")
            ^
    /fridaAnlzAp/clang-var//test_in/test_main.cpp:2:9: warning: VFIR_inserted [-W#pragma-messages]
    #pragma message("VFIR_inserted")
            ^
    2 warnings generated.
    """
    
    # 编译 varBE 的runtime
    clang++ -I $PrjHm/runtime_cpp__vars_fn/include   -c   $PrjHm/runtime_cpp__vars_fn/runtime_cpp__vars_fn.cpp
    
    #链接时，加入runtime_cpp__vars_fn.o
    clang++ test_main.o runtime_cpp__vars_fn.o -o test_main.elf
    # 插入varBE后测试材料 正常链接
    
    ./test_main.elf && echo test_main__ok
    # 插入varBE后测试材料 正常运行 ， 以下是 varBE中的destroy方法的输出
    """
    /fridaAnlzAp/clang-var/test_in/test_main.cpp:20:15,varDeclGroupCnt=0,varCntSum=0
    /fridaAnlzAp/clang-var/test_in/test_main.cpp:64:32,varDeclGroupCnt=4,varCntSum=4
    vd:{varTypeName=class MyClass,varCnt=1}
    vd:{varTypeName=struct Point,varCnt=1}
    vd:{varTypeName=struct Point,varCnt=1}
    vd:{varTypeName=struct Point,varCnt=1}
    /fridaAnlzAp/clang-var/test_in/test_main.cpp:29:15,varDeclGroupCnt=2,varCntSum=2
    vd:{varTypeName=class UserEntity,varCnt=1}
    vd:{varTypeName=struct Point,varCnt=1}
    test_main__ok
    """

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,3 +1,4 @@
+#pragma message("VarBE_inserted")
 #pragma message("VFIR_inserted")
 #define CASE_SAME(x) case x:
 #define INT_T int
@@ -16,21 +17,21 @@ public:
     double m_d1;
     int m_n2;
 
-    void ff1() {   return; /* voidFnEndInsertRet: */}
-    MyClass( ){
-return; /* voidFnEndInsertRet: */}
+    void ff1() {_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::ff1", 19, 16); /* 初始化函数变量列表, */   destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
+    MyClass( ){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::MyClass", 20, 15); /* 初始化函数变量列表, */
+destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
 
     MyClass(float f1, int n1)
             :m_d1(f1),m_n2(n1)
-    {
+    {_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::MyClass", 25, 5); /* 初始化函数变量列表, */
         char c3=1+n1;
-    return; /* voidFnEndInsertRet: */}
+    destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
 
-    ~MyClass(){
+    ~MyClass(){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::~MyClass", 29, 15); /* 初始化函数变量列表, */
         int x,y,z;
-        UserEntity userEntity;
+        UserEntity userEntity;createVar(_vdLs, "class UserEntity", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:31,30 */ ;
 
-        Point point0;
+        Point point0;createVar(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:33,21 */ ;
         auto fn_point = [](const Point& point) {
             if(point.x>point.y)
                 return point.x+point.y;
@@ -39,16 +40,16 @@ return; /* voidFnEndInsertRet: */}
         };
 
         fn_point(point0);
-    return; /* voidFnEndInsertRet: */}
+    destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
 
-    void voidDemo(int cnt, short chr){
-    return; /* voidFnEndInsertRet: */}
+    void voidDemo(int cnt, short chr){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::voidDemo", 44, 38); /* 初始化函数变量列表, */
+    destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
 
-    void voidDemo2(UserEntity userEntity){
+    void voidDemo2(UserEntity userEntity){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "MyClass::voidDemo2", 47, 42); /* 初始化函数变量列表, */
         if(true){
-            return;
+            destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return;
         }
-    return; /* voidFnEndInsertRet: */}
+    destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
 
 
 
@@ -56,20 +57,20 @@ return; /* voidFnEndInsertRet: */}
 
 int MyClass::ZERO=0;
 
-void voidDemo3(){
+void voidDemo3(){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "voidDemo3", 59, 17); /* 初始化函数变量列表, */
     int k=0;
     k++;
 
-return; /* voidFnEndInsertRet: */}
-int main(int argc, char** argv){
-    MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
+destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return; /* voidFnEndInsertRet: */}
+int main(int argc, char** argv){_VarDeclLs * _vdLs=_init_varLs_inFn("/fridaAnlzAp/clang-var/test_in/test_main.cpp", "main", 64, 32); /* 初始化函数变量列表, */
+    MyClass varMyClass;createVar(_vdLs, "class MyClass", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:65,23 */ ;
+    static Point pnt1;createVar(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:66,22 */ ;
+    struct Point pnt2;createVar(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:67,22 */ ;
     {
         struct Point * ptr1=&pnt1;
         struct Point * ptr2=&pnt2;
-        struct Point pnt3;
+        struct Point pnt3;createVar(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clang-var/test_in/test_main.cpp:71,26 */ ;
     }
 
-    return 0;
+    destroyVarLs_inFn(_vdLs); /* 销毁函数变量列表: */return 0;
 }
