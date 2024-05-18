2efa3bc770b87999976e36ad965049227405b462
prgrmz07 <prgrmz07@163.com>
2024-05-18 17:53:55 +0800

    被 添加return语句插件（voidFnEndInsertRet）处理过的输入例子

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,3 +1,4 @@
+#pragma message("VFIR_inserted")
 #define CASE_SAME(x) case x:
 #define INT_T int
 typedef double DOUBLE_typedef;
@@ -15,13 +16,14 @@ public:
     double m_d1;
     int m_n2;
 
-    MyClass( ){}
+    void ff1() { return; }
+    MyClass( ){return; /* voidFnEndInsertRet: */}
 
     MyClass(float f1, int n1)
             :m_d1(f1),m_n2(n1)
     {
         char c3=1+n1;
-    }
+    return; /* voidFnEndInsertRet: */}
 
     ~MyClass(){
         int x,y,z;
@@ -36,18 +38,18 @@ public:
         };
 
         fn_point(point0);
-    }
+    return; /* voidFnEndInsertRet: */}
 
     void voidDemo(int cnt, short chr){
         auto user_auto_var = UserEntity();
         auto user_auto_ptr=new UserEntity();
-    }
+    return; /* voidFnEndInsertRet: */}
 
     void voidDemo2(UserEntity userEntity){
         if(true){
             return;
         }
-    }
+    return; /* voidFnEndInsertRet: */}
 
 
 
@@ -59,7 +61,7 @@ void voidDemo3(){
     int k=0;
     k++;
 
-}
+return; /* voidFnEndInsertRet: */}
 int main(int argc, char** argv){
     MyClass varMyClass;
     static Point pnt1;
