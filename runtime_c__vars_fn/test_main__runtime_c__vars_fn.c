ac443da382669e186fd941e80b7cb289ddefcf1d
prgrmz07 <prgrmz07@163.com>
2024-05-20 17:47:49 +0800

    [6]【草稿中】c运行时 test_main__runtime_c__vars_fn.c

--- runtime_c__vars_fn/test_main__runtime_c__vars_fn.c
+++ runtime_c__vars_fn/test_main__runtime_c__vars_fn.c
@@ -1,18 +1,19 @@
 #include "runtime_c__vars_fn.h"
 
-class T_User{};
-class T_Book{};
-class T_Auth{};
+struct T_User{};
+struct T_Book{};
+struct T_Auth{};
+#define TRUE 1
 
 void func1(){_VarDeclLs * _vdLs=_init_varLs_inFn("runtime_c__vars_fn/test_main__runtime_c__vars_fn.c","func1",7,14);
     int a, b, c, *d;
-    T_User user1, user2, *ptr_user3; createVar(_vdLs,"T_User", 2);
+    struct T_User user1, user2, *ptr_user3; createVar(_vdLs,"T_User", 2);
 
-    if(true){
+    if(TRUE){
         for(int i=1; i <= 2; i++){
-            T_Book book1, book2, *ptr_book3,book4; createVar(_vdLs,"T_Book", 3);
+            struct T_Book book1, book2, *ptr_book3,book4; createVar(_vdLs,"T_Book", 3);
             {
-                T_Auth auth1,auth2,*ptr_auth3; createVar(_vdLs,"T_Auth", 2);
+                struct T_Auth auth1,auth2,*ptr_auth3; createVar(_vdLs,"T_Auth", 2);
             }
         }
     }
