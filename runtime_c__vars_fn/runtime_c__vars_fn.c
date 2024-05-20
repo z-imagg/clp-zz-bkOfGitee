a6027a69e16f23f8a7bfdd2fafb285e3c9f1d511
prgrmz07 <prgrmz07@163.com>
2024-05-20 17:46:04 +0800

    [6]【草稿中】c运行时 runtime_c__vars_fn.c

--- runtime_c__vars_fn/runtime_c__vars_fn.c
+++ runtime_c__vars_fn/runtime_c__vars_fn.c
@@ -1,18 +1,19 @@
 
 #include "runtime_c__vars_fn.h"
+#include <malloc.h>
 
+#define _NEW_(Type)  ( (Type*)malloc(sizeof(Type)) )
+#define _DEL_(ptr)   free(ptr); ptr=NULL
 
-
-#include <iostream>
-#include <algorithm>
-#include <numeric>
+#include "sds.h"
+#include "list.h"
 
 //函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
-_VarDeclLs *  _init_varLs_inFn(std::string srcFilePath, std::string funcName,int funcLBrc_line,int funcLBrc_column){
+_VarDeclLs *  _init_varLs_inFn(sds srcFilePath, sds funcName,int funcLBrc_line,int funcLBrc_column){
     //new:HeapObj_vdLs
-    _VarDeclLs *vdLs = new _VarDeclLs;
+    _VarDeclLs *vdLs = _NEW_(_VarDeclLs); // 分配 对象0 : _NEW_0  //new _VarDeclLs;
     //new:HeapObj_vdVec
-    vdLs->_vdVec=new std::vector<_VarDecl>();
+    vdLs->_vdVec=list_new();//new std::vector<_VarDecl>();
 
     vdLs->srcFilePath=srcFilePath;
     vdLs->funcName=funcName;
@@ -24,46 +25,42 @@ _VarDeclLs *  _init_varLs_inFn(std::string srcFilePath, std::string funcName,int
 
 
 //结构体变量声明末尾 插入 'createVar(_varLs_ptr,"变量类型名",变量个数);'
-void createVar(_VarDeclLs *vdLs, std::string varTypeName,int varCnt){
+void createVar(_VarDeclLs *vdLs, sds varTypeName,int varCnt){
 
-    _VarDecl vd;
-    vd.varTypeName=varTypeName;
-    vd.varCnt=varCnt;
+    _VarDecl *vd= _NEW_(_VarDecl);//分配 对象1 : _NEW_1
+    vd->varTypeName=varTypeName;
+    vd->varCnt=varCnt;
 
-    vdLs->_vdVec->push_back(vd);
-
+    list_node_t *node=list_node_new(vd);
+    list_rpush(vdLs->_vdVec,node); // vdLs->_vdVec->push_back(vd);
 }
 
 
 //【销毁变量通知】 函数右花括号前 插入 'destroyVarLs_inFn(_varLs_ptr);'
 void destroyVarLs_inFn(_VarDeclLs *vdLs){
-    std::vector<_VarDecl> *_vdVec = vdLs->_vdVec;
+    list_t* _vdVec = vdLs->_vdVec; // std::vector<_VarDecl>
 
     _VarDecl zero; zero.varCnt=0;
 
-    const _VarDecl &sum_vd = std::accumulate(
-    _vdVec->begin(),
-    _vdVec->end(),
-    zero,
-[](const _VarDecl &a, const _VarDecl &b) {
-      _VarDecl result;
-      result.varCnt = a.varCnt + b.varCnt;
-      return result;
+    int varCntSum=0;
+
+    list_node_t *nodeK;
+    list_iterator_t *it = list_iterator_new(_vdVec, LIST_HEAD);
+    while ((nodeK = list_iterator_next(it))) {
+      puts(nodeK->val);
+      _VarDecl* vdK=(_VarDecl*)(nodeK->val); //这不是c++，这是c，无类型信息，只能做危险的强制类型转换
+      printf("vd:{varTypeName=%s,varCnt=%d\n",vdK->varTypeName,vdK->varCnt);
+      varCntSum += vdK->varCnt;
+      _DEL_(vdK);//释放 对象1 : _DEL_1
     }
-    );
+    //释放迭代器(不释放链表自身)
+    list_iterator_destroy(it);
 
-    int varCntSum=sum_vd.varCnt;
 
-    long varDeclGroupCnt = std::distance(_vdVec->begin(), _vdVec->end());
-    std::cout << vdLs->srcFilePath << ":" << vdLs->funcLBrc_line << ":" << vdLs->funcLBrc_column << ",varDeclGroupCnt=" << varDeclGroupCnt << ",varCntSum="<< varCntSum << std::endl;
+    long varDeclGroupCnt = _vdVec->len; //std::distance(_vdVec->begin(), _vdVec->end());
+    printf( "%s:%d:%d,varDeclGroupCnt=%d,varCntSum=%d\n", vdLs->srcFilePath ,  vdLs->funcLBrc_line , vdLs->funcLBrc_column ,  varDeclGroupCnt,varCntSum ) ;
 
-    std::for_each(_vdVec->begin(), _vdVec->end(), [](const _VarDecl vd){
-        std::cout<<"vd:{varTypeName="<<vd.varTypeName<<",varCnt="<<vd.varCnt<<"}"<<std::endl;
-    });
-
-    //delete:HeapObj_vdVec
-    delete vdLs->_vdVec;  vdLs->_vdVec= nullptr;
-    //delete:HeapObj_vdLs
-    delete vdLs;
+    list_destroy(vdLs->_vdVec); // 释放 对象0 : _DEL_0  // delete vdLs->_vdVec;  vdLs->_vdVec= nullptr;
+    _DEL_(vdLs); // delete vdLs;
 
 }
