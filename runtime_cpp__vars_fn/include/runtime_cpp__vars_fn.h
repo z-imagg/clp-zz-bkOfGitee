15f363233a23142eb01f4a4195e258fb66698977
prgrmz07 <prgrmz07@163.com>
2024-05-21 02:33:57 +0800

    黑尝试2

--- runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
+++ runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
@@ -2,37 +2,41 @@
 #ifndef runtime_cpp__vars_fn_H
 #define runtime_cpp__vars_fn_H
 
+extern "C++" {
+
 #include <vector>
 #include <string>
-struct __VarDecl{
+struct __VarDecl {
 
 
-    //业务字段
-    std::string varTypeName;
-    int varCnt;
+  //业务字段
+  std::string varTypeName;
+  int varCnt;
 };
 typedef struct __VarDecl _VarDecl;
 
-struct __VarDeclLs{
-    //定位字段
-    std::string srcFilePath;
-    std::string funcName;
-    int funcLBrc_line;
-    int funcLBrc_column;
+struct __VarDeclLs {
+  //定位字段
+  std::string srcFilePath;
+  std::string funcName;
+  int funcLBrc_line;
+  int funcLBrc_column;
 
-    //业务字段
-    std::vector<_VarDecl> * _vdVec;
+  //业务字段
+  std::vector<_VarDecl> *_vdVec;
 };
 typedef struct __VarDeclLs _VarDeclLs;
 
 //TODO  函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
-_VarDeclLs *  _init_varLs_inFn(std::string srcFilePath, std::string funcName,int funcLBrc_line,int funcLBrc_column);
+_VarDeclLs *_init_varLs_inFn(std::string srcFilePath, std::string funcName, int funcLBrc_line, int funcLBrc_column);
 
 
 //结构体变量声明末尾 插入 'createVar(_varLs_ptr,"变量类型名",变量个数);'
-void createVar(_VarDeclLs * _vdLs, std::string varTypeName,int varCnt);
+void createVar(_VarDeclLs *_vdLs, std::string varTypeName, int varCnt);
 
 
 //【销毁变量通知】 TODO  函数右花括号前 插入 'destroyVarLs_inFn(_varLs_ptr);'
-void destroyVarLs_inFn(_VarDeclLs * _vdLs);
+void destroyVarLs_inFn(_VarDeclLs *_vdLs);
+
+}
 #endif //runtime_cpp__vars_fn_H
