ce6d8671c469bb67b857d7b0a0a16611d997dcf6
prgrmz07 <prgrmz07@163.com>
2024-05-24 10:42:25 +0800

    C多线程测试main

--- runtime_c__TmPnt_ThreadLocal/TestMain_C.c
+++ runtime_c__TmPnt_ThreadLocal/TestMain_C.c
@@ -2,13 +2,56 @@
 
 #include <stdio.h>
 
-void func1(){
-TL_TmPnt__update(4);
+void* thread_function(void* _tmPnt_init_ptr);
+
+#include <pthread.h>
+int main(int argc, char** argv){
+  pthread_t thread1, thread2, thread3;
+
+  int _tmPnt_init_1 = 4, _tmPnt_init_2 = 7, _tmPnt_init_3 = 23;
+  // 创建新线程
+  if (pthread_create(&thread1, NULL, thread_function, &_tmPnt_init_1 )) {
+    perror("Error_creating_thread_1\n");
+    return 1;
+  }
+
+  // 创建新线程
+  if (pthread_create(&thread2, NULL, thread_function,  &_tmPnt_init_2 )) {
+    perror("Error_creating_thread_2\n");
+    return 1;
+  }
+
+  // 创建新线程
+  if (pthread_create(&thread3, NULL, thread_function, &_tmPnt_init_3 )) {
+    perror("Error_creating_thread_3\n");
+    return 1;
+  }
+
+
+  char tmp;
+  printf("pressEnterToExit:");scanf("%c",&tmp);
+
+  return 0;
 }
 
-int main(int argc, char** argv){
-func1();
-int tmPnt=TL_TmPnt__get();
-printf("tmPnt=%d\n",tmPnt);
+#include <unistd.h>
 
+extern __thread  int TL_TmPnt;
+void* thread_function(void* _tmPnt_init_ptr) {
+  int* tmPnt_init_ptr = (int*) _tmPnt_init_ptr;
+  int tmPnt_init = *tmPnt_init_ptr;
+
+  int tmPnt_ThreadLocal_A=TL_TmPnt__get();
+  TL_TmPnt__update(tmPnt_init);
+
+  int tmPnt_ThreadLocal_B=TL_TmPnt__get();
+
+  sleep(1);
+  TL_TmPnt__update(tmPnt_init*2);
+  int tmPnt_ThreadLocal_C=TL_TmPnt__get();
+  int tmPnt_ThreadLocal_D=TL_TmPnt;
+
+  printf("ThreadLocal_TmPnt@0x%x; tmPnt_ThreadLocal[A=%d, B=%d, C=%d, D=%d]; ThreadLocal_TmPnt=%d\n",&TL_TmPnt, tmPnt_ThreadLocal_A, tmPnt_ThreadLocal_B, tmPnt_ThreadLocal_C, tmPnt_ThreadLocal_D, TL_TmPnt);
+
+  pthread_exit(NULL);
 }
