71576fba40eaa4f2f60c4d60131679d265f216ad
prgrmz07 <prgrmz07@163.com>
2024-05-24 10:21:57 +0800

    正常编译

--- runtime_c__TmPnt_ThreadLocal/include/rntm_c__TmPnt_ThrLcl.h
+++ runtime_c__TmPnt_ThreadLocal/include/rntm_c__TmPnt_ThrLcl.h
@@ -2,11 +2,19 @@
 #ifndef rntm_c__TmPnt_ThrLcl_H
 #define rntm_c__TmPnt_ThrLcl_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
+
 void TL_TmPnt__update(int _TmPnt_new);
 
 int TL_TmPnt__get() ;
-}
+
+#ifdef __cplusplus
+};
+#endif
+
+
 
 
 #endif //rntm_c__TmPnt_ThrLcl_H
