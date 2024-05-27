68dfd1033c54e13135e0d43331a44d274af46ed7
prgrmz07 <prgrmz07@163.com>
2024-05-27 08:48:39 +0800

    [2] c++运行时接口函数新名

--- runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
+++ runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
@@ -32,16 +32,16 @@ struct __VarDeclLs{
 };
 typedef struct __VarDeclLs _VarDeclLs;
 
-//   函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn();'
-_VarDeclLs *  _init_varLs_inFn(std::string srcFilePath, std::string funcName,int funcLBrc_line,int funcLBrc_column);
+//   函数左花括号后 插入 'void* _varLs_ptr=_init_varLs_inFn__RtCxx();'
+_VarDeclLs *  _init_varLs_inFn__RtCxx(std::string srcFilePath, std::string funcName, int funcLBrc_line, int funcLBrc_column);
 
 
-//结构体变量声明末尾 插入 'createVar(_varLs_ptr,"变量类型名",变量个数);'
-void createVar(_VarDeclLs * _vdLs, std::string varTypeName,int varCnt);
+//结构体变量声明末尾 插入 'createVar__RtCxx(_varLs_ptr,"变量类型名",变量个数);'
+void createVar__RtCxx(_VarDeclLs * _vdLs, std::string varTypeName, int varCnt);
 
 
-//【销毁变量通知】 TODO  函数右花括号前 插入 'destroyVarLs_inFn(_varLs_ptr);'
-void destroyVarLs_inFn(_VarDeclLs * _vdLs);
+//【销毁变量通知】  函数右花括号前 插入 'destroyVarLs_inFn__RtCxx(_varLs_ptr);'
+void destroyVarLs_inFn__RtCxx(_VarDeclLs * _vdLs);
 
 #endif  //__cplusplus判断结束
 
