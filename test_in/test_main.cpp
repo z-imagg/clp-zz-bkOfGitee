ae442dda1e4c0a80725572d4b35bf23454032251
prgrmz07 <prgrmz07@163.com>
2024-06-07 21:23:34 +0800

    测试材料

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,73 +1,32 @@
-#define CASE_SAME(x) case x:
-#define INT_T int
-typedef double DOUBLE_typedef;
+//来源 ： /fridaAnlzAp/sleuthkit-4.12.1/tsk/fs/apfs.cpp
 
-struct Point{
-    float x;
-    double y;
-};
-class UserEntity{};
-class ScoreRelation{};
-class MyClass {
+#include <stdexcept>
+
+class APFSPool{};
+class apfs_block_num{};
+class APFSObject{
 public:
-    static int ZERO;
-public:
-    double m_d1;
-    int m_n2;
-
-    void ff1() {   }
-    MyClass( ){}
-
-    MyClass(float f1, int n1)
-            :m_d1(f1),m_n2(n1)
-    {
-        char c3=1+n1;
-    }
-
-    ~MyClass(){
-        int x,y,z;
-        UserEntity userEntity;
-
-        Point point0;
-        auto fn_point = [](const Point& point) {
-            if(point.x>point.y)
-                return point.x+point.y;
-            else
-                return 0.1;
-        };
-
-        fn_point(point0);
-    }
-
-    void voidDemo(int cnt, short chr){
-    }
-
-    void voidDemo2(UserEntity userEntity){
-        if(true){
-            return;
-        }
-    }
-
-
+    APFSObject(const APFSPool& pool,
+                    const apfs_block_num block_num);
 
 };
+class APFSSpacemanCIB :APFSObject{
+public:
+    APFSSpacemanCIB(const APFSPool& pool,
+                                     const apfs_block_num block_num);
+};
 
-int MyClass::ZERO=0;
-
-void voidDemo3(){
-    int k=0;
-    k++;
-
+int obj_type(){
+    return 0;
+}
+APFSSpacemanCIB::APFSSpacemanCIB(const APFSPool& pool,
+                                 const apfs_block_num block_num)
+        : APFSObject(pool, block_num) {
+    if (obj_type() != 0) {
+        throw std::runtime_error("APFSSpacemanCIB: invalid object type");
+    }
+//这里不该插入 销毁变量调用 destroyVarLs_inFn__RtC**
 }
 int main(int argc, char** argv){
-    MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
-    {
-        struct Point * ptr1=&pnt1;
-        struct Point * ptr2=&pnt2;
-        struct Point pnt3;
-    }
-
     return 0;
 }
