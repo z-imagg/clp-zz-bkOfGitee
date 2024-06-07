049f53ead812daea8ae3368e6a66b8e5aeca5144
prgrmz07 <prgrmz07@163.com>
2024-06-07 22:16:40 +0800

    测试材料 简化

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,32 +1,9 @@
-#pragma message("VFIR_inserted")
-//来源 ： /fridaAnlzAp/sleuthkit-4.12.1/tsk/fs/apfs.cpp
+#include "ClassDef.h"
 
-#include <stdexcept>
-
-class APFSPool{};
-class apfs_block_num{};
-class APFSObject{
-public:
-    APFSObject(const APFSPool& pool,
-                    const apfs_block_num block_num);
-
-};
-class APFSSpacemanCIB :APFSObject{
-public:
-    APFSSpacemanCIB(const APFSPool& pool,
-                                     const apfs_block_num block_num);
-};
-
-int obj_type(){
-    return 0;
-}
-APFSSpacemanCIB::APFSSpacemanCIB(const APFSPool& pool,
-                                 const apfs_block_num block_num)
-        : APFSObject(pool, block_num) {
-    if (obj_type() != 0) {
-        throw std::runtime_error("APFSSpacemanCIB: invalid object type");
+C1::C1(int i ) {
+    const auto sz =
+            0;
+    if (true) {
+        throw false;
     }
-return; /* voidFnEndInsertRet: */}
-int main(int argc, char** argv){
-    return 0;
-}
+    return; /* voidFnEndInsertRet: */}
