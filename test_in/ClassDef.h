c663617b1aa7a5504a1e94af4ef803a3953f86e1
prgrmz07 <prgrmz07@163.com>
2024-06-07 22:17:49 +0800

    [测试材料]  c++构造函数(声明在.h、实现在.cxx)时,TraverseCXXMethodDecl并不会遍此构造函数实现
    c++构造函数声明和实现分开放(声明在.h、实现在.cxx)时,TraverseCXXMethodDecl并不会遍历这样的构造函数实现

---
+++ test_in/ClassDef.h
@@ -0,0 +1,5 @@
+
+class C1{
+public:
+    C1(int i);
+};
--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,73 +1,9 @@
-#define CASE_SAME(x) case x:
-#define INT_T int
-typedef double DOUBLE_typedef;
+#include "ClassDef.h"
 
-struct Point{
-    float x;
-    double y;
-};
-class UserEntity{};
-class ScoreRelation{};
-class MyClass {
-public:
-    static int ZERO;
-public:
-    double m_d1;
-    int m_n2;
-
-    void ff1() {   }
-    MyClass( ){}
-
-    MyClass(float f1, int n1)
-            :m_d1(f1),m_n2(n1)
-    {
-        char c3=1+n1;
+C1::C1(int i ) {
+    const auto sz =
+            0;
+    if (true) {
+        throw false;
     }
-
-    ~MyClass(){
-        int x,y,z;
-        UserEntity userEntity;
-
-        Point point0;
-        auto fn_point = [](const Point& point) {
-            if(point.x>point.y)
-                return point.x+point.y;
-            else
-                return 0.1;
-        };
-
-        fn_point(point0);
-    }
-
-    void voidDemo(int cnt, short chr){
-    }
-
-    void voidDemo2(UserEntity userEntity){
-        if(true){
-            return;
-        }
-    }
-
-
-
-};
-
-int MyClass::ZERO=0;
-
-void voidDemo3(){
-    int k=0;
-    k++;
-
-}
-int main(int argc, char** argv){
-    MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
-    {
-        struct Point * ptr1=&pnt1;
-        struct Point * ptr2=&pnt2;
-        struct Point pnt3;
-    }
-
-    return 0;
-}
+    return; /* voidFnEndInsertRet: */}
