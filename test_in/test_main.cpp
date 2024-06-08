161179eb8ac9310410053912bf3a07aa03df6e86
prgrmz07 <prgrmz07@163.com>
2024-06-08 16:48:18 +0800

    [材料结果] 结论： 简单遍历Decl, 能进入lambda表达式内部

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,23 +1,24 @@
+#pragma message("VarBE_inserted")
 #include "MyClz.h"
 #include <algorithm>
 #include <list>
 
 
-int main(int argc, char** argv){
-    my_nsp::MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
+int main(int argc, char** argv){_VarDeclLs * _vdLs=_init_varLs_inFn__RtCxx("/fridaAnlzAp/clp-zz/test_in/test_main.cpp", "main", 6, 32); /* 初始化函数变量列表, */
+    my_nsp::MyClass varMyClass;createVar__RtCxx(_vdLs, "my_nsp::MyClass", 1)  /* 创建变量通知,  /fridaAnlzAp/clp-zz/test_in/test_main.cpp:7,31 */ ;
+    static Point pnt1;createVar__RtCxx(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clp-zz/test_in/test_main.cpp:8,22 */ ;
+    struct Point pnt2;createVar__RtCxx(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clp-zz/test_in/test_main.cpp:9,22 */ ;
     {
         struct Point * ptr1=&pnt1;
         struct Point * ptr2=&pnt2;
-        struct Point pnt3;
+        struct Point pnt3;createVar__RtCxx(_vdLs, "struct Point", 1)  /* 创建变量通知,  /fridaAnlzAp/clp-zz/test_in/test_main.cpp:13,26 */ ;
     }
 
 
-    std::list<my_nsp::MyClass*> ls;
+    std::list<my_nsp::MyClass*> ls;createVar__RtCxx(_vdLs, "std::list<my_nsp::MyClass *>", 1)  /* 创建变量通知,  /fridaAnlzAp/clp-zz/test_in/test_main.cpp:17,35 */ ;
 
-  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; return k; });
+  std::for_each(ls.begin(), ls.end(), [](my_nsp::MyClass* k){ my_nsp::MyClass* idx=k; destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return k; });
 
-    return 0;
+    destroyVarLs_inFn__RtCxx(_vdLs); /* 销毁函数变量列表: */return 0;
 }
 
